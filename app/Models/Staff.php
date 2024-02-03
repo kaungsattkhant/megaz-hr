5ccd38c633a58b741ef58ee87dec4186868fc63c
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-02-03 10:24:43 +0630

    Update Staff.php

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -3,16 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Hash;
+
+use Laravel\Sanctum\HasApiTokens;
 
 use App\Models\Department;
 use App\Models\Gender;
 use App\Models\Role;
 
-class Staff extends Model
+class Staff extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
     protected $fillable=[
         'name','phone_number','nrc_number','address','gender_id','department_id','is_active'
