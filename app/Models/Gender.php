32bc3f88e83644cf13ad05d5bbf91274cc2b4d0b
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-02-14 15:07:40 +0630

    Update Gender.php

--- app/Models/Gender.php
+++ app/Models/Gender.php
@@ -15,6 +15,16 @@ class Gender extends BaseModel
         'name'
     ];
 
+    public function getCreatedAt()
+    {
+        return parent::getCreatedAt();
+    }
+
+    public function getUpdatedAt()
+    {
+        return parent::getUpdatedAt();
+    }
+
     public function staffs()
     {
         return $this->hasMany(Staff::class);
