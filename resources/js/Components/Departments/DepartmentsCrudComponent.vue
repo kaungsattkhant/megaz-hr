7b74a1436cee5fefc34adc62c006931ed4624893
Nevarnar <loverhein2011@gmail.com>
2024-02-17 10:41:34 +0630

    fix delete

--- resources/js/Components/Departments/DepartmentsCrudComponent.vue
+++ resources/js/Components/Departments/DepartmentsCrudComponent.vue
@@ -44,7 +44,9 @@
                                     {{ department.name }}
                                 </td>
                                 <td class="whitespace-nowrap px-6 py-4">
-                                    <button id="edit-btn" class="pr-1">
+                                    <button @click="deleteBtnClicked(department.id)"
+                                    data-te-toggle="modal" data-te-target="#deleteModal"
+                                    id="edit-btn" class="pr-1">
                                         <i class="fas fa-trash-alt"></i>
                                     </button>
                                 </td>
@@ -108,6 +110,83 @@
         </div>
 
 
+
+
+
+
+
+
+        <!--Delete Modal -->
+        <div
+        data-te-modal-init
+        class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+        id="deleteModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div
+            data-te-modal-dialog-ref
+            class="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]">
+                <div
+                class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none ">
+                <div
+                    class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 ">
+                    <!--Modal title-->
+                    <h5
+                    class="text-xl font-medium leading-normal text-neutral-800 "
+                    id="exampleModalLabel">
+                    Delete ?
+                    </h5>
+                    <!--Close button-->
+                    <button
+                    type="button"
+                    class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                    data-te-modal-dismiss
+                    aria-label="Close">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="h-6 w-6">
+                        <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    </button>
+                </div>
+
+                <!--Modal body-->
+                <div class="relative flex-auto p-4" data-te-modal-body-ref>
+                    <p>
+                        Are you sure ?
+                    </p>
+                </div>
+
+                <!--Modal footer-->
+                <div
+                    class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 ">
+                    <button
+                    type="button"
+                    class="inline-block px-6 pb-2 pt-2.5 text-xs focus:outline-none focus:ring-0 "
+                    data-te-modal-dismiss
+                    >
+                        Close
+                    </button>
+                    <button @click="confirmDeleteBtnClicked"
+                    type="button" data-te-toggle="modal" data-te-target="#deleteModal"
+                    class="ml-1 inline-block rounded bg-red-600 px-6 pb-2 pt-2.5 text-xs  text-white   focus:outline-none focus:ring-0 "
+                    >
+                        Delete
+                    </button>
+                </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 
 </template>
@@ -123,6 +202,7 @@
 
                 departmentList: [],
                 name: null,
+                deleteId: null,
 
 
                 per_page: 10,
@@ -166,6 +246,19 @@
                     alert('some errors occur');
                 }
             },
+
+            deleteBtnClicked(id){
+                this.deleteId = id;
+            },
+
+            async confirmDeleteBtnClicked(){
+                let url = `/api/departments/${this.deleteId}`;
+                alert(url)
+                let response = await deleteApiData({url: url});
+                if(response.success){
+                    alert(`deleted`);
+                }
+            }
 
         },
         mounted()
