81d43ced017e8b0fb535252c9dee60f2e4fc7f5a
Nevarnar <loverhein2011@gmail.com>
2024-02-19 10:47:53 +0630

    roomandtable , service crud

---
+++ resources/views/tables&rooms/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('page_title', 'Table')
+@section('table', 'active-link')
+@section('content')
+    <div id="app">
+        <tables-crud-component/>
+
+    </div>
+
+@endsection
