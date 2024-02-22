9132a415b2364f6b9ec745bb06906dcf0df7c55e
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-02-22 09:53:52 +0630

    Inventory ledger list fixed

--- resources/views/inventories/inventory_ledger.blade.php
+++ resources/views/inventories/inventory_ledger.blade.php
@@ -1,13 +1,9 @@
 @extends('layouts.main')
-
 @section('page_title', 'Inventories')
 @section('inventories', 'active-link')
+
 @section('content')
     <div id="app">
         <inventory-ledgers-component :inventory_id="{{ $inventory_id}}"/>
-
-        
-
     </div>
-
 @endsection
