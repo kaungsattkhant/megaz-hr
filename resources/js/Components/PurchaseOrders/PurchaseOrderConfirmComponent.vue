362a50a9cf6ff0ccae97f10855156b5f68cfc3de
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-02-29 12:20:27 +0630

    Fix po confirm front end

--- resources/js/Components/PurchaseOrders/PurchaseOrderConfirmComponent.vue
+++ resources/js/Components/PurchaseOrders/PurchaseOrderConfirmComponent.vue
@@ -78,14 +78,14 @@
                                 {{ (purchaseOrderItem.amount * purchaseOrderItem.quantity).toLocaleString() }}
                             </td>
                             <td class=" px-6 py-4 font-medium ">
-                                <button @click="removePurchaseOrderItemBtnClicked(purchaseOrderItemsIndex)">
+                                <button @click="editPurchaseOrderItemBtnClicked(purchaseOrderItemsIndex)" data-te-toggle="modal" data-te-target="#editModal">
                                     <i class="fal fa-pencil  pr-3"></i>
                                 </button>
-                                <button @click="removePurchaseOrderItemBtnClicked(purchaseOrderItemsIndex)">
+                                <button >
                                     <i class="fal fa-check  pr-3"></i>
                                 </button>
-                                <button @click="removePurchaseOrderItemBtnClicked(purchaseOrderItemsIndex)">
-                                    <i class="fal fa-times  pr-3"></i>
+                                <button @click="removePurchaseOrderItemBtnClicked(purchaseOrderItemsIndex)" data-te-toggle="modal" data-te-target="#deleteModal">
+                                    <i class="fal fa-times pr-3"></i>
                                 </button>
                             </td>
                         </tr>
@@ -103,9 +103,108 @@
             </button>
         </div>
     </div>
+
+    <!--Delete Modal -->
+    <div data-te-modal-init class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+        id="deleteModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div data-te-modal-dialog-ref class="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px]
+            items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7
+            min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]">
+            <div class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col
+                rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none ">
+                <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 ">
+                    <!--Modal title-->
+                    <h5 class="text-xl font-medium leading-normal text-neutral-800 " id="exampleModalLabel">
+                        Delete ?
+                    </h5>
+                    <!--Close button-->
+                    <button type="button" class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none" data-te-modal-dismiss aria-label="Close">
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+
+                <!--Modal body-->
+                <div class="relative flex-auto p-4" data-te-modal-body-ref>
+                    <p>
+                        Are you sure ?
+                    </p>
+                </div>
+
+                <!--Modal footer-->
+                <div class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 ">
+                    <button type="button" class="inline-block px-6 pb-2 pt-2.5 text-xs focus:outline-none focus:ring-0 " data-te-modal-dismiss>
+                        Close
+                    </button>
+                    <button @click="confirmRemovePurchaseOrderItemBtnClicked" type="button" data-te-toggle="modal" data-te-target="#deleteModal"
+                    class="ml-1 inline-block rounded bg-red-600 px-6 pb-2 pt-2.5 text-xs  text-white   focus:outline-none focus:ring-0 ">
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div data-te-modal-init class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+        id="editModal" tabindex="-1" aria-labelledby="create_modalLabel" aria-hidden="true">
+        <div data-te-modal-dialog-ref class="pointer-events-none relative w-auto mb-12 translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
+            <div class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none">
+                <div class="relative  p-4">
+                    <!--Modal title-->
+                    <h5 class="text-xl text-center mt-2 font-medium leading-normal text-black" id="create_modalLabel">
+                        Edit
+                    </h5>
+                    <!--Close button-->
+                    <button type="button" class="absolute top-4 right-4 focus:shadow-none focus:outline-none"
+                        data-te-modal-dismiss aria-label="Close">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="h-5 w-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+
+                <!--Modal body-->
+                <div class="relative px-12 py-4" data-te-modal-body-ref>
+                    <div class="mb-4">
+                        <label for="" class="block text-sm text-black mb-3">
+                            Quantity
+                        </label>
+                        <input type="number" placeholder="Quantity" v-model="editQuantity" class="text-sm border border-gray-300 input-ui w-full bg-transparent rounded-lg focus:ring-0">
+                    </div>
+                    <div class="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+                        <input
+                            class="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                            type="checkbox"
+                            value=""
+                            id="checkboxDefault" />
+                        <label
+                            class="inline-block pl-[0.15rem] hover:cursor-pointer"
+                            for="checkboxDefault">
+                            Later Buy
+                        </label>
+                    </div>
+                </div>
+
+                <!--Modal footer-->
+                <div class="flex justify-center px-12 mb-6">
+                    <button type="button" @click="confirmEditPurchaseOrderItemBtnClicked" class="add-btn focus:outline-none focus:ring-0 "
+                        data-te-toggle="modal" data-te-target="#editModal">
+                        Edit
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import { Modal, initTE } from "tw-elements";
     import { getApiData, postApiData, deleteApiData } from '../../utilities/ajax-helpers';
 
     export default {
@@ -118,6 +217,11 @@
                 selectedItem: null,
                 quantity: null,
                 purchaseOrderItems: [],
+                deleteId: null,
+                deleteIndex: null,
+
+                editQuantity: 0,
+                editPurchaseOrderItem: null,
             };
         },
 
@@ -164,18 +268,41 @@
                 this.quantity = null;
             },
 
-            async removePurchaseOrderItemBtnClicked(purchaseOrderItemsIndex){
-                console.log(this.purchaseOrderItems[purchaseOrderItemsIndex]);
+            removePurchaseOrderItemBtnClicked(purchaseOrderItemsIndex){
                 if(this.purchaseOrderItems[purchaseOrderItemsIndex].id){
-                    let url = `/api/purchase_orders_items/${this.purchaseOrderItems[purchaseOrderItemsIndex].id}`;
+                    this.deleteId = this.purchaseOrderItems[purchaseOrderItemsIndex].id;
+                }
+                this.deleteIndex = purchaseOrderItemsIndex;
+            },
+
+            async confirmRemovePurchaseOrderItemBtnClicked(){
+                if(this.deleteId){
+                    let url = `/api/purchase_orders_items/${this.deleteId}`;
                     let response = await deleteApiData({url: url});
                     if(response.success){
-                        this.purchaseOrderItems.splice(purchaseOrderItemsIndex, 1);
+                        this.purchaseOrderItems.splice(this.deleteIndex, 1);
                     }
                 }
                 else{
-                    this.purchaseOrderItems.splice(purchaseOrderItemsIndex, 1);
+                    this.purchaseOrderItems.splice(this.deleteIndex, 1);
                 }
+                this.deleteId = null;
+                this.deleteIndex = null;
+            },
+
+            editPurchaseOrderItemBtnClicked(purchaseOrderItemsIndex){
+                this.editPurchaseOrderItem = this.purchaseOrderItems[purchaseOrderItemsIndex];
+                this.editQuantity = this.editPurchaseOrderItem.quantity;
+            },
+
+            confirmEditPurchaseOrderItemBtnClicked(){
+                let index = this.purchaseOrderItems.findIndex(poItem => poItem.item_id == this.editPurchaseOrderItem.item_id);
+                if(index != -1){
+                    this.purchaseOrderItems[index].quantity = this.editQuantity;
+                }
+                console.log(this.purchaseOrderItems[index]);
+                this.editQuantity = 0;
+                this.editPurchaseOrderItem = null;
             },
 
             async createPurchaseOrderBtnClicked(){
@@ -217,7 +344,7 @@
         },
 
         mounted(){
-
+            initTE({Modal});
         }
     }
 </script>
