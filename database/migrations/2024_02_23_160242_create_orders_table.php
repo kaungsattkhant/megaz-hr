0220e9366742d7e95fe952bb24d405f237d122e2
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-02-29 15:26:21 +0630

    Update 2024_02_23_160242_create_orders_table.php

--- database/migrations/2024_02_23_160242_create_orders_table.php
+++ database/migrations/2024_02_23_160242_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('order');
+            $table->unsignedBigInteger('order_id');
             $table->integer('total_quantity');
             $table->dateTime('date');
             $table->integer('total');
