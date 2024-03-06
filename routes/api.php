5f9ca76dc09868b13dc36633ed99da9e2c86cc6f
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-03-06 11:55:46 +0630

    Inactive room list api endpoint update

--- routes/api.php
+++ routes/api.php
@@ -184,7 +184,7 @@ Route::post('/menus/{id}/add_price', [MenuAPIController::class, 'addPriceToMenu'
 
 Route::get('/rooms',[EntityAPIController::class,'getRoomsWithInvoice']);
 Route::get('/rooms/{id}',[EntityAPIController::class,'getRoomDetail']);
-Route::get('/rooms/inactive',[EntityAPIController::class,'getOnlyInactiveRooms']);
+Route::get('/rooms/lists/inactive',[EntityAPIController::class,'getOnlyInactiveRooms']);
 
 Route::post('/rooms/start',[InvoiceAPIController::class,'startRoomSession']);
 Route::post('/rooms/orders',[OrderAPIController::class,'addOrder']);
