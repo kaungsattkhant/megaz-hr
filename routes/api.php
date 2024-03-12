cc54011b8c088f5daa30d38ebb6fc71163c67000
ksk-nexbe <email@example.com>
2024-03-12 13:38:52 +0630

    fix:api

--- routes/api.php
+++ routes/api.php
@@ -111,7 +111,7 @@ Route::middleware('auth:api')->group(function(){
     Route::resource('head_accounts', HeadAccountController::class)->only(['index','store','show','destroy']);
     Route::resource('sub_accounts', SubAccountController::class)->only(['index','store','show','destroy']);
     Route::resource('accounts', AccountController::class)->only(['index','store','show','destroy']);
-});git a
+});
 
 
 Route::controller(ExcelImportController::class)->group(function(){
