a744bade7866b440ccc67bc1f393e97de0da9bec
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-03-19 17:03:12 +0630

    Transaction list bind frontend

--- resources/js/Components/FinancialTransaction/FinancialTransactionCrudComponent.vue
+++ resources/js/Components/FinancialTransaction/FinancialTransactionCrudComponent.vue
@@ -555,7 +555,7 @@
                 let url = `/api/transactions`;
                 let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
                 if(response.success){
-
+                    window.location.reload();
                 }
             },
 
@@ -581,7 +581,6 @@
                 let url = `/api/transaction_confirmed`;
                 let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
                 if(response.success){
-                    // alert(response.message);
                     window.location.reload();
                 }
 
