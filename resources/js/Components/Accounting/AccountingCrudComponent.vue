a8397cf7e216000e6fc118eb0a10bdc28cfecbd7
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-04-02 16:27:46 +0630

    Search and active on off done

--- resources/js/Components/Accounting/AccountingCrudComponent.vue
+++ resources/js/Components/Accounting/AccountingCrudComponent.vue
@@ -70,7 +70,16 @@
                                     <input
                                     :checked="account.is_active == 1"
                                     @change="isActiveToggled(account.id)"
-                                    class="me-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-black/25 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-switch-2 after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ms-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-switch-1 checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-switch-3 focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ms-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-switch-3 checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-white/25 dark:after:bg-surface-dark dark:checked:bg-primary dark:checked:after:bg-primary"
+                                    class="me-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-black/25 before:pointer-events-none before:absolute before:h-3.5
+                                    before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5
+                                    after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-switch-2 after:transition-[background-color_0.2s,transform_0.2s]
+                                    after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ms-[1.0625rem]
+                                    checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-switch-1
+                                    checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:before:scale-100
+                                    focus:before:opacity-[0.12] focus:before:shadow-switch-3 focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s]
+                                    focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-['']
+                                    checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ms-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-switch-3
+                                    checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-white/25 dark:after:bg-surface-dark dark:checked:bg-primary dark:checked:after:bg-primary"
                                     type="checkbox"
                                     role="switch"/>
                                 </td>
@@ -362,7 +371,6 @@
                 editId: null,
                 editIndex: null,
 
-                isActive: true,
                 deleteId: null,
                 deleteIndex: null,
 
@@ -527,33 +535,37 @@
                 let index = this.accountList.findIndex(account => account.id == id);
                 if(index != -1){
                     if(this.accountList[index].is_active == 1){
-                        this.accountList[index].is_active == 0;
+                        this.accountList[index].is_active = 0;
                     }
                     else{
-                        this.accountList[index].is_active == 1;
+                        this.accountList[index].is_active = 1;
                     }
 
-                    console.log(this.accountList[index]);
+                    let url = `/api/is_active`;
+                    let formData = new FormData();
+                    formData.append('id', id);
+                    formData.append('type', 'account');
+                    let response = postApiData({url: url, form_data: formData, token: this.getToken()});
                 }
             },
 
-            deleteBtnClicked(id, index){
-                this.deleteId = id;
-                this.deleteIndex = index;
-            },
+            // deleteBtnClicked(id, index){
+            //     this.deleteId = id;
+            //     this.deleteIndex = index;
+            // },
 
-            async confirmDeleteBtnClicked(){
-                let formData = new FormData();
-                formData.append("id", this.deleteId);
-                formData.append("type", "account");
-                let response = await postApiData({ url: `/api/is_active`, form_data: formData, token: this.getToken() });
-                if(response.data){
-                    this.accountList.splice(this.deleteIndex, 1);
-                }
+            // async confirmDeleteBtnClicked(){
+            //     let formData = new FormData();
+            //     formData.append("id", this.deleteId);
+            //     formData.append("type", "account");
+            //     let response = await postApiData({ url: `/api/is_active`, form_data: formData, token: this.getToken() });
+            //     if(response.data){
+            //         this.accountList.splice(this.deleteIndex, 1);
+            //     }
 
-                this.deleteId = null;
-                this.deleteIndex = null;
-            },
+            //     this.deleteId = null;
+            //     this.deleteIndex = null;
+            // },
 
             async searchBtnClicked(){
                 let url = null;
