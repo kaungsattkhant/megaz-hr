631504c13846d090a2374389bb60e760957ba884
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-04-02 09:11:17 +0630

    menu with prices

--- app/Http/Controllers/API/MenuAPIController.php
+++ app/Http/Controllers/API/MenuAPIController.php
@@ -46,7 +46,8 @@ class MenuAPIController extends Controller
 
     public function menuByMenuCategory(int $id)
     {
-        $menu = Menu::where('menu_category_id',$id)->get();
+        $menu = Menu::where('menu_category_id',$id)->with('prices')->get();
+
         ResponseData($menu);
 
     }
