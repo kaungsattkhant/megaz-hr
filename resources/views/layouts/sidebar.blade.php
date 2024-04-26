2036a28a63505af8d73b9a86c0a0534b9d037421
ksk-nexbe <email@example.com>
2024-04-26 21:31:07 +0630

    fix:po item left

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -215,13 +215,7 @@
                                 </a>
                             </li>
 
-                            <li>
-                                <a href="{{ route('purchase_orders.confirm_poitems') }}"
-                                    class="flex items-center @yield('confirm_purchase_order_items')">
-                                    <i class="fal fa-truck-loading  pr-3"></i>
-                                    Confirm Purchase Order Items
-                                </a>
-                            </li>
+
 
                             <li>
                                 <a href="{{ route('fixed_assets.index') }}"
@@ -231,7 +225,15 @@
                                 </a>
                             </li>
                         @endif
-
+                        @if (checkDepartmentAndRoles('Inventory', ['Staff']))
+                            <li>
+                                <a href="{{ route('purchase_orders.confirm_poitems') }}"
+                                    class="flex items-center @yield('confirm_purchase_order_items')">
+                                    <i class="fal fa-truck-loading  pr-3"></i>
+                                    Confirm Purchase Order Items
+                                </a>
+                            </li>
+                        @endif
                     </ul>
 
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,9 @@ Route::middleware(['departments:HR,Finance,Management'])->group(function () {
     Route::view('/purchase_orders/create', 'purchase_orders.create')->name('purchase_orders.create');
     Route::view('/purchase_orders/{poId}/confirm', 'purchase_orders.confirm')->name('purchase_orders.confirm');
     Route::view('/purchase_orders/{poId}/buy', 'purchase_orders.buy')->name('purchase_orders.buy');
+});
+Route::middleware(['departments:Inventory'])->group(function () {
+
     Route::view('/confirm_purchase_order_items', 'purchase_orders.confirm_poitems')->name('purchase_orders.confirm_poitems');
 });
 
@@ -89,7 +92,7 @@ Route::view('/suppliers/{id}/edit', 'supplier.edit')->name('suppliers.edit');
 Route::view('/fixed_assets', 'fixed_assets.index')->name('fixed_assets.index');
 
 //pos
-Route::group(['prefix'=>'pos'], function(){
+Route::group(['prefix' => 'pos'], function () {
     Route::view('/login', 'pos.auth.index')->name('pos.login');
     Route::middleware(['departments:Catering'])->group(function () {
         Route::view('/home', 'pos.home.index')->name('pos.index');
