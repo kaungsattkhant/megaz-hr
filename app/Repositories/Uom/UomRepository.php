108093cb490164a77369038c77b6e609484ea6df
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-05-07 13:11:08 +0630

    order item with menu and room

--- app/Repositories/Uom/UomRepository.php
+++ app/Repositories/Uom/UomRepository.php
@@ -33,6 +33,30 @@ class UomRepository implements UomRepositoryInterface
         }
     }
 
+    // public function uomConversaionList(Request $request)
+    // {
+    //     if ($request->per_page || $request->page) {
+    //         $totalCount = UomConversion::count();
+    //         $pageNumber = 1;
+    //         $perPage = 20;
+    //         if ($request->page) {
+    //             $pageNumber = $request->page;
+    //         }
+    //         if ($request->per_page) {
+    //             $perPage = $request->per_page;
+    //         }
+    //         $skip = ($pageNumber - 1) * $perPage;
+    //         $uoms = Uom::skip($skip)->take($perPage)->get();
+    //         $paginationData = MakePaginationData($request, $totalCount, 'uoms');
+    //         $paginationData['uoms'] = $uoms;
+
+    //         return $paginationData;
+    //     } else {
+    //         $uoms = Uom::all();
+    //         return $uoms;
+    //     }
+    // }
+
     public function createUomConversion(array $data)
     {
         DB::beginTransaction();
