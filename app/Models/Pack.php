5067087a327615dae55628d47c07640d8471d5e4
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-05-11 15:21:38 +0630

    pack done

---
+++ app/Models/Pack.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pack extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'menu_id','date','expired_at','created_by','expired_at','created_by','status'
+    ];
+}
