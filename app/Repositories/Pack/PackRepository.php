47794dfc5c071dd47a933c22aa3437eb3693d4df
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-05-20 11:30:59 +0630

    pack with inventory

--- app/Repositories/Pack/PackRepository.php
+++ app/Repositories/Pack/PackRepository.php
@@ -2,6 +2,9 @@
 
 namespace App\Repositories\Pack;
 
+use App\Http\Action\Inventory\StoreInventory;
+use App\Models\InventoryLedgerItem;
+use App\Models\Item;
 use App\Models\Menu;
 use App\Models\Pack;
 use App\Models\PackItem;
@@ -18,7 +21,6 @@ class PackRepository implements PackRepositoryInterface
             $menu = Menu::find($data['menu_id']);
             $menuItems = $menu->items;
 
-
              for ($i = 0; $i < $data['quantity']; $i++) {
                 $pack = Pack::create([
                     'menu_id' => $data['menu_id'],
@@ -27,12 +29,37 @@ class PackRepository implements PackRepositoryInterface
                     'created_by' => $data['created_by'],
                     'status' => 'not yet'
                 ]);
+                $inventoryId = UserData()->department->inventory->inventory_id;
+                $inventoryLedger = (new StoreInventory($inventoryId))->storeToInventoryLedger($pack, 'pack', 'out');
+
                 foreach ($menuItems as $item) {
+                    $itemInventories = InventoryLedgerItem::where('item_id', $item->id)->get();
+                    $enterInventoryValue = 0;
+                    $outInventroyValue = 0;
+
+                    foreach ($itemInventories as $itemInventory) {
+                        if ($itemInventory->inventory_ledger->action == 'in') {
+                            $enterInventoryValue += $itemInventory->quantity;
+                        } else if ($itemInventory->inventory_ledger->action == 'out') {
+                            $outInventroyValue += $itemInventory->quantity;
+                        }
+                    }
+                    $stockInInventory = $enterInventoryValue - $outInventroyValue;
+                    if($stockInInventory < $item->pivot->weight)
+                    {
+                        ResponseMessage('Stock is not enough',402);
+                    }
                     $packItem = PackItem::create([
                         'pack_id' => $pack->id,
                         'item_id' => $item->id,
                         'uom_id' => $item->pivot->uom_id,
                         'quantity' => $item->pivot->weight
+                    ]);
+
+                    $inventoryLedger->inventory_ledger_items()->create([
+                        'item_id' => $item->id,
+                        'quantity' => $item->pivot->weight,
+                        'inventory_ledger_id' => $inventoryLedger->id,
                     ]);
                 }
             }
