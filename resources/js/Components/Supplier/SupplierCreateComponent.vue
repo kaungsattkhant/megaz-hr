1d1317d08c777c1da38b0693e574760847cdeb30
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-05-30 16:11:18 +0630

    Update SupplierCreateComponent.vue

--- resources/js/Components/Supplier/SupplierCreateComponent.vue
+++ resources/js/Components/Supplier/SupplierCreateComponent.vue
@@ -207,7 +207,7 @@ export default {
             let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
             if(response.success){
                 // console.log(response);
-                // window.location.replace("/suppliers");
+                window.location.replace("/suppliers");
             }
         },
     },
