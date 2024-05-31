1d84d1c4142889103e8bd76155cd5d92ef207415
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-05-31 10:03:46 +0630

    revert temp

--- routes/web.php
+++ routes/web.php
@@ -159,7 +159,7 @@ Route::middleware(['departments:fixed-asset'])->group(function () {
 //pos
 Route::group(['prefix' => 'pos'], function () {
     Route::view('/login', 'pos.auth.index')->name('pos.login');
-    // Route::middleware(['departments:pos'])->group(function () {
+    Route::middleware(['departments:pos'])->group(function () {
         Route::view('/home', 'pos.home.index')->name('pos.index');
         Route::view('/customer', 'pos.customers.index')->name('pos.customers');
         Route::view('/customer/create', 'pos.customers.create')->name('pos.customers.create');
@@ -168,7 +168,7 @@ Route::group(['prefix' => 'pos'], function () {
         Route::view('/cashbook/detail', 'pos.cashbook.detail')->name('pos.cashbooks.detail');
         Route::view('/invoices', 'pos.invoices.index')->name('pos.invoices');
         Route::view('/invoices/detail', 'pos.invoices.detail')->name('pos.invoices.detail');
-    // });
+    });
 });
 
 
