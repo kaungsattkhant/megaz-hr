c532cfe1a7f0f354c5ccf3ef4bec57be3fecb02b
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-07-05 13:26:41 +0630

    Update PosNotificationComponent.vue

--- resources/js/Components/Pos/Common/PosNotificationComponent.vue
+++ resources/js/Components/Pos/Common/PosNotificationComponent.vue
@@ -150,7 +150,7 @@
 
 
             let channelName = `room-notification-request.${this.department.id}`;
-            let eventName = `App\Events\RoomNotificationRequest`;
+            let eventName = `RoomNotificationRequest`;
 
             this.listenBroadCastNotifications(channelName, eventName);
 
