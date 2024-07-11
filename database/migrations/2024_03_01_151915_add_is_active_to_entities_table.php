5566d33dccb2cdf3988825c5159ebc97aa743d95
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-07-11 09:50:15 +0630

    typo fix

--- database/migrations/2024_03_01_151915_add_is_active_to_entities_table.php
+++ database/migrations/2024_03_01_151915_add_is_active_to_entities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('entities', function (Blueprint $table) {
             //
-            $table->boolean('is_active')->after('is_eailable')->default(0);
+            $table->boolean('is_active')->after('is_available')->default(0);
             $table->string('status')->after('is_active')->default('inactive');
         });
     }
