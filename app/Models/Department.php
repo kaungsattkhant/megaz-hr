82ea16baf50c40579cd9868994cf2f76df5e0e91
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-07-15 11:00:16 +0630

    department with areas

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -65,4 +65,9 @@ class Department extends BaseModel
     {
         return $this->belongsToMany(Feature::class,'department_feature');
     }
+
+    public function areas()
+    {
+        return $this->hasMany(Area::class);
+    }
 }
