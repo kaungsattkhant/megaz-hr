9696b27d7d5c79d689f055c6c8a8c56b97d6c127
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-07-16 21:36:50 +0630

    update

--- routes/api.php
+++ routes/api.php
@@ -254,6 +254,9 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/uom_conversions/{id}',[UomAPIController::class,'updateUomConversion']);
     Route::get('/uom_conversions',[UomAPIController::class,'getUomConversionList']);
 
+    Route::get('/bookings',[BookingAPIController::class,'bookingList']);
+    Route::post('/bookings',[BookingAPIController::class,'createBooking']);
+
 
 });
 
@@ -403,10 +406,6 @@ Route::controller(CustomerLevelDiscountAPIController::class)->group(function ()
     Route::post('/customer_level_discounts/{id}','updateCustomerLevelDiscount');
     Route::delete('/customer_level_discounts/{id}','deleteCustomerLevelDiscount');
 });
-
-Route::get('/bookings',[BookingAPIController::class,'bookingList']);
-Route::post('/bookings',[BookingAPIController::class,'createBooking']);
-
 
 Route::post('send_notification', [NotificationController::class, 'sendNotification']);
 
