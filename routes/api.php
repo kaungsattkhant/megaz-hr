bced69c96cf736bc9655e0b972c2a50d1505765a
ksk-nexbe <email@example.com>
2024-07-16 17:56:57 +0630

    merge:pks

--- routes/api.php
+++ routes/api.php
@@ -130,10 +130,8 @@ Route::middleware('auth:api')->group(function () {
         Route::put('/purchase_orders/{id}', 'updatePurchaseOrder');
         Route::get('/purchase_orders/{purchase_order}', 'detail');
         Route::delete('/purchase_orders/{id}', 'deletePurchaseOrder');
-
         Route::get('/purchase_orders_items', 'getPurchaseOrderItem');
         Route::post('/purchase_orders_items', 'createPurchaseOrderItem');
-
         Route::delete('/purchase_orders_items/{id}', 'deletePurchaseOrderItem');
         Route::post('purchase_orders_bought', 'boughtPurchaseOrder');
         Route::post('updateIsCheck', 'updateIsCheck');
@@ -300,11 +298,16 @@ Route::delete('/staffs/{staff_id}/inventories/{inventory_id}',[StaffAPIControlle
 Route::delete('/staffs/{staff_id}/features/{feature_id}',[StaffAPIController::class,'deleteFeatureStaff']);
 
 
-Route::get('/tasks', [TaskController::class, 'getTaskData']);
-Route::post('/tasks', [TaskController::class, 'createTask']);
-Route::put('/tasks/{id}', [TaskController::class, 'updateTask']);
-Route::delete('/tasks/{id}', [TaskController::class, 'deleteTask']);
-
+// Route::get('/tasks', [TaskController::class, 'getTaskData']);
+// Route::post('/tasks', [TaskController::class, 'createTask']);
+// Route::put('/tasks/{id}', [TaskController::class, 'updateTask']);
+// Route::delete('/tasks/{id}', [TaskController::class, 'deleteTask']);
+Route::controller(TaskController::class)->group(function () {
+    Route::get('/tasks', 'getTaskData');
+    Route::post('/tasks', 'createTask');
+    Route::put('/tasks/{id}', 'updateTask');
+    Route::delete('/tasks/{id}','deleteTask');
+});
 Route::get('/complaints', [ComplaintAPIController::class, 'getComplainData']);
 
 Route::get('/entities', [EntityAPIController::class, 'getEntityData']);
