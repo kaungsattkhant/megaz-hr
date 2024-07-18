de2fc697a663c9b86d8bd206a603ed25fd19e12a
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-07-18 12:10:25 +0630

    Update TasksCrudComponent.vue

--- resources/js/Components/Tasks/TasksCrudComponent.vue
+++ resources/js/Components/Tasks/TasksCrudComponent.vue
@@ -377,9 +377,8 @@
                 let formData = new FormData();
                 formData.append('name', this.name);
                 formData.append('description', this.tasks);
-                if(this.selectedArea){
-                    formData.append('area_id', this.selectedArea.id);
-                }
+                formData.append('area_id', this.selectedArea.id);
+
                 formData.append('role_id', this.selectedRole.id);
                 formData.append('assigned_days', this.selectedDays);
                 let response = await postApiData({url: '/api/tasks', form_data: formData, token: this.getToken()});
