4e125b1e579fcb70db2f79401773e99787f22294
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-07-25 15:38:32 +0630

    menu with original price

--- app/Repositories/FoodOrder/FoodOrderRepository.php
+++ app/Repositories/FoodOrder/FoodOrderRepository.php
@@ -13,9 +13,18 @@ class FoodOrderRepository implements FoodOrderRepositoryInterface
     public function listAllData(Request $request)
     {
         $validateDate = $request->date ?? CurrentDate();
-        $foodOrders = FoodOrder::with(['customer', 'customer.addresses' => function ($query) {
-            $query->where('is_default', 1);
-        }, 'confirmedBy', 'cancelledBy', 'foodOrderItems.menu.areas'])
+        $foodOrders = FoodOrder::where('status', '!=', 'cancelled')->with([
+            'customer',
+            'customer.addresses' => function ($query) {
+                $query->where('is_default', 1);
+            },
+            'confirmedBy',
+            'cancelledBy',
+            'foodOrderItems' => function ($query) {
+                $query->where('status', '!=', 'cancelled')
+                    ->with('menu.areas');
+            }
+        ])
             ->whereBetween('date_time', [$validateDate . ' 00:00:00', $validateDate . ' 23:59:59'])
             ->orderBy('created_at', 'desc')
             ->get();
@@ -29,15 +38,46 @@ class FoodOrderRepository implements FoodOrderRepositoryInterface
         DB::beginTransaction();
         try {
             $foodOrderItem = FoodOrderItem::find($id);
-            $foodOrderItem->area_id = $request->area_id;
-            $foodOrderItem->status = "confirmed";
-            $foodOrderItem->save();
-            DB::commit();
+            if ($request->is_confirm == 1) {
+                $foodOrderItem->status = 'confirmed';
+                $foodOrderItem->save();
+            } else {
+                $foodOrderItem->status = 'cancelled';
+                $foodOrderItem->save();
+            }
 
+            DB::commit();
             ResponseData($foodOrderItem);
         } catch (\Exception $e) {
             DB::rollBack();
-            ResponseMessage($e->getMessage());
+            ResponseMessage($e->getMessage(),422);
+            throw $e;
+        }
+    }
+
+    public function confirmFoodOrder(int $id,Request $request)
+    {
+        DB::beginTransaction();
+        try{
+            $foodOrder = FoodOrder::find($id);
+            if($request->is_confirm == 1)
+            {
+                $foodOrder->status = 'confirmed';
+                $foodOrder->confirmed_by = UserData()->id;
+                $foodOrder->confirmed_at = CurrentTime();
+                $foodOrder->save();
+            }else{
+                $foodOrder->status = 'cancelled';
+                $foodOrder->cancelled_by = UserData()->id;
+                $foodOrder->cancelled_at = CurrentTime();
+                $foodOrder->save();
+            }
+            DB::commit();
+
+        }catch(\Exception $e)
+        {
+            DB::rollBack();
+            ResponseMessage($e->getMessage(),422);
             throw $e;
         }
     }
