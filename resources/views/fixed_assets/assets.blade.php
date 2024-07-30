ac589f9862310e2789db3156f00a06095666e56b
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-07-30 10:10:31 +0630

    temp

---
+++ resources/views/fixed_assets/assets.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('page_title', 'Assets')
+@section('fixed_asset', 'active-link')
+@section('content')
+
+<div id="app">
+    <asset-crud-component/>
+</div>
+
+@endsection
