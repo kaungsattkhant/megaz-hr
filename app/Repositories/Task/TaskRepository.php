df7fc65f25eab0faa238c7afad8e684e90e8a689
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-08-05 17:36:56 +0630

    task

--- app/Repositories/Task/TaskRepository.php
+++ app/Repositories/Task/TaskRepository.php
@@ -57,7 +57,7 @@ class TaskRepository implements TaskRepositoryInterface
                 $perPage = $request->per_page;
             }
             $skip = ($pageNumber - 1) * $perPage;
-            $tasks = Task::where('is_active', 1)->skip($skip)->take($perPage)->with(['role.department'])->get();
+            $tasks = Task::where('is_active', 1)->skip($skip)->take($perPage)->with(['role.department', 'task_details','task_details.completedBy','task_details.doubleCheckedBy'])->get();
             $paginationData = MakePaginationData($request, $totalCount, 'tasks');
             $paginationData['tasks'] = $tasks;
 
