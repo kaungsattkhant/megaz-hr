067d7c517c359839a87e59d69bce2ec5ef8efcef
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-08-07 15:58:33 +0630

    asset item

--- database/migrations/2024_07_25_095935_create_asset_items_table.php
+++ database/migrations/2024_07_25_095935_create_asset_items_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('asset_items', function (Blueprint $table) {
             $table->id();
             $table->char('name',120);
+            $table->string('item_code')->unique();
             $table->unsignedInteger('second_account_id');
             $table->unsignedInteger('second_depreciation_account_id');
             $table->unsignedInteger('created_by');
