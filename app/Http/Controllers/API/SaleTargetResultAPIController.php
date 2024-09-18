b36fa071725dd074b18d2ad7f54ed5541df2fd4f
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-09-18 09:48:40 +0630

    megaz

--- app/Http/Controllers/API/SaleTargetResultAPIController.php
+++ app/Http/Controllers/API/SaleTargetResultAPIController.php
@@ -14,17 +14,14 @@ class SaleTargetResultAPIController extends Controller
 {
     public function getSaleTargetResult(Request $request)
     {
-        // Get the role ID of the user
         $role = UserData()->roles->first();
         $roleId = $role->id;
 
-        // Handle the 'month' query parameter (format: YYYY-MM)
-        $currentMonth = $request->get('month', Carbon::now()->format('Y-m')); // Default to current month
+        $currentMonth = $request->get('month', Carbon::now()->format('Y-m'));
         $parsedMonth = Carbon::createFromFormat('Y-m', $currentMonth);
         $year = $parsedMonth->year;
         $month = $parsedMonth->month;
 
-        // Get target amount for the specified month and role
         $targetAmount = SaleTargetPosition::whereYear('month', $year)
             ->whereMonth('month', $month)
             ->whereHas('targetPositions', function ($query) use ($roleId) {
@@ -38,15 +35,12 @@ class SaleTargetResultAPIController extends Controller
             ->collapse()
             ->sum('amount');
 
-        // Get total amount from TargetPositionResult for the specified month and role
         $totalAmount = TargetPositionResult::whereYear('date_time', $year)
             ->whereMonth('date_time', $month)
             ->where('role_id', $roleId)
             ->sum('amount');
 
-        // Handle specific departments
         if (in_array(UserData()->department_id, [5, 7, 8])) {
-            // Get target menus for the specified month
             $targetMenus = SaleTargetMenu::whereYear('month', $year)
                 ->whereMonth('month', $month)
                 ->with(['targetMenus' => function ($query) {
@@ -57,14 +51,12 @@ class SaleTargetResultAPIController extends Controller
                 ->collapse()
                 ->keyBy('menu_id');
 
-            // Get sold menus for the specified month
             $soldMenus = TargetMenuResult::whereYear('date_time', $year)
                 ->whereMonth('date_time', $month)
                 ->with('menu')
                 ->get()
                 ->keyBy('menu_id');
 
-            // Combine target and sold menu results
             $allMenus = $soldMenus->union($targetMenus)->map(function ($menu, $menuId) use ($targetMenus, $soldMenus) {
                 return [
                     'menu' => $soldMenus[$menuId]->menu ?? $targetMenus[$menuId]->menu,
@@ -73,10 +65,9 @@ class SaleTargetResultAPIController extends Controller
                 ];
             });
         } else {
-            $allMenus = collect(); // Empty for other departments
+            $allMenus = collect();
         }
 
-        // Prepare response data
         $responseData = [
             'target_position_result' => [
                 'target_amount' => $targetAmount,
@@ -87,7 +78,6 @@ class SaleTargetResultAPIController extends Controller
             'target_menu_result' => $allMenus->values()->all(),
         ];
 
-        // Return response
         ResponseData($responseData);
     }
 
