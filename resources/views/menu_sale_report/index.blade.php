d8cc7e4c3c7da40ea93afcb94932d86ed9d1a29f
Nevarnar <loverhein2011@gmail.com>
2024-10-08 10:11:15 +0630

    fix

---
+++ resources/views/menu_sale_report/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('page_title', 'Menu Sale Report')
+@section('menu_sale_report', 'active-link')
+@section('content')
+    <div id="app">
+        <menu-sale-report-component/>
+
+    </div>
+
+@endsection
