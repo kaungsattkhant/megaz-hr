0daa50f4f29a5aa923392470bb0237daa2d2b1f0
Nyi_Min_Htut <nyiminhtut2003@gmail.com>
2024-10-08 12:35:49 +0630

    branch nmh sidebar added

--- resources/views/pos/orderItem/index.blade.php
+++ resources/views/pos/orderItem/index.blade.php
@@ -1,7 +1,7 @@
 @extends('pos.layouts.main')
 
 @section('page_title', 'Order')
-@section('foodOrders', 'pos-active-link')
+@section('posOrder', 'pos-active-link')
 @section('content')
 <div id="app">
     <pos-order-item-list/>
