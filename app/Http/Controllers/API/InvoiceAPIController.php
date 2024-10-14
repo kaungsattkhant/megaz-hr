e99f6399d3e19bfafc5d1ead1e755ecb3278e9bc
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-10-14 22:23:02 +0630

    Update InvoiceAPIController.php

--- app/Http/Controllers/API/InvoiceAPIController.php
+++ app/Http/Controllers/API/InvoiceAPIController.php
@@ -105,7 +105,7 @@ class InvoiceAPIController extends Controller
         $entity = Entity::find($latestRoomSession->entitySession->entity_id);
 
         if (isset($request->waiter)) {
-            $managerRole = Role::where('name', 'Manager')
+            $managerRole = Role::where('name', 'Staff')
                 ->where('department_id', $catering_department->id)
                 ->first();
 
