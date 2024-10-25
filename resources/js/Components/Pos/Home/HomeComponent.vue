ade0d9e06b566626a296234c71ad739d196ac512
Nevarnar <loverhein2011@gmail.com>
2024-10-25 15:30:08 +0630

    fix

--- resources/js/Components/Pos/Home/HomeComponent.vue
+++ resources/js/Components/Pos/Home/HomeComponent.vue
@@ -1129,14 +1129,15 @@
                     this.selectedAreaId = this.areaList[0].id
                     console.log('area id = ' + this.selectedAreaId)
                     // this.getRoomList();
-                    this.areaType = response.data.area_type
+                    this.areaType = response.data[0].area_type.type
                     this.callTest();
-                    // if(this.areaType == 'bar_and_restaurant'){
-                    //     this.$refs.posTable.getTableList(response.data[0].id)
-                    // }
-                    // else{
-                    //     this.$refs.posRoom.getRoomList(response.data[0].id)
-                    // }
+                    if(this.areaType == 'bar_and_restaurant'){
+                        this.$refs.posTable.getTableList(response.data[0].id)
+                        console.log('why no table?')
+                    }
+                    else{
+                        this.$refs.posRoom.getRoomList(response.data[0].id)
+                    }
                 }
             },
             btnClickedArea(areaId,areaType){
@@ -1144,13 +1145,13 @@
                 // this.isShowSidebar = false
                 // this.getRoomList();
                 this.areaType = areaType
-                console.log(areaType , areaId)
-                // if(this.areaType == 'bar_and_restaurant'){
-                //     this.$refs.posTable.getTableList(areaId)
-                // }
-                // else{
-                //     this.$refs.posRoom.getRoomList(areaId)
-                // }
+                // console.log(areaType , areaId)
+                if(areaType == 'bar_and_restaurant'){
+                    this.$refs.posTable.getTableList(areaId)
+                }
+                else{
+                    this.$refs.posRoom.getRoomList(areaId)
+                }
             },
 
 
