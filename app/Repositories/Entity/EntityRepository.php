f623ae6de8841658deb8b48229e670d8c4d7a5f6
kaungsattkhant <ookaung42@gmail.com>
2024-11-05 20:59:11 +0630

    done

--- app/Repositories/Entity/EntityRepository.php
+++ app/Repositories/Entity/EntityRepository.php
@@ -236,8 +236,10 @@ class EntityRepository implements EntityRepositoryInterface
             }
             // }
             // dd($total_accessory_value);
+            // $entitySession->roomSession->invoice=$invoice;
             $entitySession['start_date'] = $firstRoomSession->start_date;
             $entitySession['end_date'] = $lastRoomSession->end_date;
+            $entitySession['invoice']=$invoice;
             //service add response
             $entitySession['services'] = $invoiceServiceCollection;
             $entitySession['invoice_accessories'] = $invoiceAccessories;
