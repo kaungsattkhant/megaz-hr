050503831343c062e2d37f8e01656c1fc16a8872
kaungsattkhant <ookaung42@gmail.com>
2024-11-06 10:22:42 +0630

    fix:accessory update

--- app/Http/Requests/Admin/AccessoryCreateRequest.php
+++ app/Http/Requests/Admin/AccessoryCreateRequest.php
@@ -14,7 +14,7 @@ class AccessoryCreateRequest extends APIRequest
         return [
             'code' => [
                 'required',
-                Rule::unique('accessories')->ignore($this->accessory),
+                Rule::unique('accessories')->ignore($id),
             ],
             'price' => $id === null ? ['required'] : [], // Make 'price' required only if $id is null
             'name' => 'required',
