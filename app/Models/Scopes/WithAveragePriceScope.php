d89600d5cd152e71d0ea7b80110926687b35da00
kaungsattkhant <ookaung42@gmail.com>
2024-12-03 21:50:33 +0630

    fix:average price

--- app/Models/Scopes/WithAveragePriceScope.php
+++ app/Models/Scopes/WithAveragePriceScope.php
@@ -22,28 +22,40 @@ class WithAveragePriceScope implements Scope
         })
         ->join('uoms as base_uom', 'items.base_uom_id', 'base_uom.id')
         ->join('uoms as item_uom', 'items.uom_id', 'item_uom.id')
-
-        ->addSelect(
-            'items.*',      
+        ->addSelect([
+            'items.*',
             'base_uom.name as base_uom_name',
             'item_uom.name as item_uom',
             'uom_conversions.conversion as uom_conversion',
-            DB::raw('
-                CAST((
-                    SELECT COALESCE(AVG(latest_prices.price), 0) 
-                    FROM (
-                        SELECT ip.price 
-                        FROM supplier_items si
-                        JOIN item_prices ip ON si.id = ip.supplier_item_id
-                        WHERE si.item_id = items.id
-                        AND ip.id = (
-                            SELECT MAX(sub_ip.id)
-                            FROM item_prices sub_ip
-                            WHERE sub_ip.supplier_item_id = si.id
-                        )
-                    ) AS latest_prices
-                ) AS DECIMAL(10,2)) AS average_price
-            ')
-        );
+            'average_price' => function ($subQuery) {
+                $subQuery->selectRaw('COALESCE(AVG(ip.price), 0)')
+                    ->from('supplier_items as si')
+                    ->join('item_prices as ip', 'si.id', '=', 'ip.supplier_item_id')
+                    ->whereColumn('si.item_id', 'items.id')
+                    ->whereRaw('ip.id = (SELECT MAX(sub_ip.id) FROM item_prices sub_ip WHERE sub_ip.supplier_item_id = si.id)');
+            }
+        ]);
+        // ->addSelect(
+        //     'items.*',      
+        //     'base_uom.name as base_uom_name',
+        //     'item_uom.name as item_uom',
+        //     'uom_conversions.conversion as uom_conversion',
+        //     DB::raw('
+        //         CAST((
+        //             SELECT COALESCE(AVG(latest_prices.price), 0) 
+        //             FROM (
+        //                 SELECT ip.price 
+        //                 FROM supplier_items si
+        //                 JOIN item_prices ip ON si.id = ip.supplier_item_id
+        //                 WHERE si.item_id = items.id
+        //                 AND ip.id = (
+        //                     SELECT MAX(sub_ip.id)
+        //                     FROM item_prices sub_ip
+        //                     WHERE sub_ip.supplier_item_id = si.id
+        //                 )
+        //             ) AS latest_prices
+        //         ) AS DECIMAL(10,2)) AS average_price
+        //     ')
+        // );
     }
 }
