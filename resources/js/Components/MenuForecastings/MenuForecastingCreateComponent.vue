353203426c1877b9bcae9bcf7ae3c8590a0d0fa0
Nevarnar <loverhein2011@gmail.com>
2024-12-11 16:42:20 +0630

    fix

--- resources/js/Components/MenuForecastings/MenuForecastingCreateComponent.vue
+++ resources/js/Components/MenuForecastings/MenuForecastingCreateComponent.vue
@@ -150,19 +150,19 @@
             <div class=" ">
                 <ul class="mb-0 flex list-none flex-row flex-wrap border-b-0 pl-0" role="tablist" data-te-nav-ref>
                     <li role="presentation" class="group">
-                        <button
+                        <button @click="getMenuTableList()"
                             class="z-20 mt-2 block px-7 pb-3.5 pt-3 rounded-tr-md rounded-tl-md font-inter text-xs font-medium  leading-tight text-neutral-500  focus:isolate data-[te-nav-active]:text-[#fff] data-[te-nav-active]:bg-[#845adf]  bg-white custom-shadow-tab relative"
                             data-te-toggle="pill" data-te-target="#tabs-menu" role="tab" data-te-nav-active
                             aria-controls="tabs-menu" aria-selected="true">Menu</button>
                     </li>
                     <li role="presentation" class="-ml-0.5 group">
-                        <a href="#tabs-material"
+                        <a href="#tabs-material" @click="getRawMaterialList()"
                             class="z-[19] mt-2 block px-7 pb-3.5 pt-3 rounded-tr-md rounded-tl-md font-inter text-xs font-medium  leading-tight text-neutral-500  focus:isolate data-[te-nav-active]:text-[#fff] data-[te-nav-active]:bg-[#845adf] bg-white custom-shadow-tab relative"
                             data-te-toggle="pill" data-te-target="#tabs-material" role="tab"
                             aria-controls="tabs-material" aria-selected="true">Raw Material</a>
                     </li>
                     <li role="presentation" class="-ml-1 group">
-                        <a href="#tabs-hr"
+                        <a href="#tabs-hr" @click="getHrList()"
                             class="z-[18] mt-2 block px-7 pb-3.5 pt-3 rounded-tr-md rounded-tl-md font-inter text-xs font-medium  leading-tight text-neutral-500  focus:isolate data-[te-nav-active]:text-[#fff] data-[te-nav-active]:bg-[#845adf] bg-white custom-shadow-tab relative"
                             data-te-toggle="pill" data-te-target="#tabs-hr" role="tab"
                             aria-controls="tabs-hr" aria-selected="true">HR</a>
@@ -204,6 +204,9 @@
                                             {{ menu.amount }}
                                         </td>
                                         <td class="">
+                                            <button data-te-toggle="modal" data-te-target="#change_quantity" @click="btnClickedEditQuantityModal(menu,menuIndex)">
+                                                <i class="fal fa-plus  pr-3"></i>
+                                            </button>
                                             <button @click="removeMenuTableItem(menuIndex)">
                                                 <i class="fal fa-trash  pr-3"></i>
                                             </button>
@@ -270,7 +273,7 @@
                                             {{ raw.name }}
                                         </td>
                                         <td class="">
-                                            <button data-te-toggle="modal" data-te-target="#create_modal">
+                                            <button data-te-toggle="modal" data-te-target="#add_raw_material" @click="btnClickPoModal(raw)">
                                                 <i class="fal fa-plus  pr-3"></i>
                                             </button>
                                         </td>
@@ -291,9 +294,6 @@
                                         <th scope="col" class="">
                                             Working Hour
                                         </th>
-                                        <th scope="col" class="">
-    
-                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -304,11 +304,6 @@
                                         </td>
                                         <td class="">
                                             {{ hr.total_working_hour }}
-                                        </td>
-                                        <td class="">
-                                            <button data-te-toggle="modal" data-te-target="#create_modal">
-                                                <i class="fal fa-plus  pr-3"></i>
-                                            </button>
                                         </td>
                                     </tr>
                                 </tbody>
@@ -324,7 +319,7 @@
             
             <div>
                 <button class="add-btn" @click="btnClickedMenuForecastingCreate()">
-                    Create QKR
+                    Create 
                 </button>
             </div>
         </div>
@@ -333,14 +328,14 @@
         <!-- Modal -->
         <div data-te-modal-init
             class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
-            id="create_modal" tabindex="-1" aria-labelledby="create_modalLabel" aria-hidden="true">
+            id="change_quantity" tabindex="-1" aria-labelledby="create_modalLabel" aria-hidden="true">
             <div data-te-modal-dialog-ref
                 class="pointer-events-none relative w-auto mb-12 translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
                 <div class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none">
                     <div class="relative flex justify-between py-2 px-6 border-b">
                         <h5 class="text-base text-center mt-2 font-semibold leading-normal font-inter"
                             id="create_modalLabel">
-                            Create Raw Material
+                            Edit Quantity
                         </h5>
                         <button type="button" class="text-xs focus:shadow-none focus:outline-none"
                             data-te-modal-dismiss aria-label="Close" id="close">
@@ -355,7 +350,49 @@
                             <label for="" class="label-form mb-3">
                                 Quantity
                             </label>
-                            <input type="text" placeholder="Particular" class="input-ui">
+                            <input type="text" placeholder="Quantity" v-model="menuQuantity" class="input-ui">
+                        </div>
+                    </div>
+                    <div class="flex justify-end gap-x-4 px-6 mb-6 pt-4">
+                        <button type="button" class="cancel-btn focus:shadow-none focus:outline-none"
+                            data-te-modal-dismiss aria-label="Close">
+                            Cancel
+                        </button>
+                        <button type="button" @click="btnClickedEditQuantity()"
+                            class="add-btn focus:outline-none focus:ring-0 ">
+                            Create
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal -->
+        <div data-te-modal-init
+            class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+            id="add_raw_material" tabindex="-1" aria-labelledby="create_modalLabel" aria-hidden="true">
+            <div data-te-modal-dialog-ref
+                class="pointer-events-none relative w-auto mb-12 translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
+                <div class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none">
+                    <div class="relative flex justify-between py-2 px-6 border-b">
+                        <h5 class="text-base text-center mt-2 font-semibold leading-normal font-inter"
+                            id="create_modalLabel">
+                            Edit Quantity
+                        </h5>
+                        <button type="button" class="text-xs focus:shadow-none focus:outline-none"
+                            data-te-modal-dismiss aria-label="Close" id="close">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="relative px-6 py-4 border-b" data-te-modal-body-ref>
+                        <div class="mb-4">
+                            <label for="" class="label-form mb-3">
+                                Quantity
+                            </label>
+                            <input type="text" placeholder="Quantity" v-model="po_quantity" class="input-ui">
                         </div>
                         <div class="mb-4">
                             <label for="" class="label-form mb-3">
@@ -363,16 +400,23 @@
                             </label>
                             <div class="bg-white mb-0 w-full text-sm inline-block"
                                 data-te-select-wrapper-ref>
-                                <select data-te-select-init data-te-select-placeholder="Select Type"
-                                    data-te-select-filter="true" name="" id="" v-model="selectedType" class="input-ui">
-                                    <option :value="type" v-for="(type, typeIndex) in typeList"
-                                        :key="typeIndex"> {{ type.name }} </option>
+                                <select data-te-select-init data-te-select-placeholder="Select UOM"
+                                    data-te-select-filter="true" name="" id="" v-model="po_uom" class="input-ui">
+                                    <option :value="uom" v-for="(uom, uomIndex) in uomList"
+                                        :key="uomIndex"> {{ uom.name }} </option>
                                 </select>
                             </div>
                         </div>
                         <div class="mb-4">
                             <label class="label-form mb-3">PO Number</label>
-                            <input type="text" placeholder="Amount" class="input-ui">
+                            <div class="bg-white mb-0 w-full text-sm inline-block"
+                                data-te-select-wrapper-ref>
+                                <select data-te-select-init data-te-select-placeholder="Select PO"
+                                    data-te-select-filter="true" name="" id="" v-model="selectedPo" class="input-ui">
+                                    <option :value="po" v-for="(po, poIndex) in poList"
+                                        :key="poIndex"> {{ po.name }} </option>
+                                </select>
+                            </div>
                         </div>
 
                     </div>
@@ -423,9 +467,22 @@ export default {
 
             minDate: "",
 
+            menuTableListSecond:[],
             rawMaterialList:[],
             hrList:[],
 
+            selectedMenuIndex:null,
+            selectedEditMenu:null,
+            menuQuantity:null, // use in quantity change modal
+
+            poList:[],
+            uomList:[],
+            itemUoms:[],
+            po_quantity:null,
+            po_uom:null,
+            selectedPo:null,
+
+            selectedItem:null,
         };
     },
 
@@ -519,29 +576,112 @@ export default {
 
         async clickedBtnCreate(){
             this.is_step = 2;
+            initTE({ Modal });
             this.getRawMaterial();
             this.getHr();
             
         },
-        async getRawMaterial(){
+        async getMenuTableList(){
             let formData = new FormData();
-            formData.append("quantity", this.quantity);
-            let url = '/api/forecast/raw_materials/' + this.selectedMenu.id;
+            formData.append("forecast_datas", JSON.stringify(this.menuTableList));
+            let url = '/api/forecast/menus';
+            let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
+            if(response.success){
+                this.menuTableListSecond = response.data;
+                initTE({ Modal });
+            }
+        },
+        btnClickedEditQuantityModal(menu,index){
+            this.selectedEditMenu = menu;
+            this.selectedMenuIndex = index;
+        },
+        async btnClickedEditQuantity(){
+            let formData = new FormData();
+            formData.append("quantity", this.menuQuantity);
+            let url = '/api/forecast/menus/' + this.selectedEditMenu.menu_id;
+            let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
+            if(response.success){
+                this.menuTableList[this.selectedMenuIndex].quantity = response.data.quantity;
+            }
+        },
+        async getRawMaterialList(){
+            let formData = new FormData();
+            formData.append("forecast_datas", JSON.stringify(this.menuTableList));
+            let url = '/api/forecast/raw_materials';
             let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
             if(response.success){
                 this.rawMaterialList = response.data;
+                initTE({ Modal });
             }
         },
-        async getHr(){
+        // async getRawMaterial(){
+        //     let formData = new FormData();
+        //     formData.append("quantity", this.quantity);
+        //     let url = '/api/forecast/raw_materials/' + this.selectedMenu.id;
+        //     let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
+        //     if(response.success){
+        //         this.rawMaterialList = response.data;
+        //     }
+        // },
+        async getHrList(){
             let formData = new FormData();
-            formData.append("quantity", this.quantity);
-            let url = '/api/forecast/hr/' + this.selectedMenu.id;
+            formData.append("forecast_datas", JSON.stringify(this.menuTableList));
+            let url = '/api/forecast/hr';
             let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
             if(response.success){
                 this.hrList = response.data;
+                initTE({ Modal });
+            }
+        },
+        // async getHr(){
+        //     let formData = new FormData();
+        //     formData.append("quantity", this.quantity);
+        //     let url = '/api/forecast/hr/' + this.selectedMenu.id;
+        //     let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
+        //     if(response.success){
+        //         this.hrList = response.data;
+        //     }
+        // },
+        btnClickPoModal(item){
+            this.selectedItem = item
+        },
+        async btnClickedCreatePo(){
+            let formData = new FormData();
+            formData.append("forecast_datas", JSON.stringify(this.menuTableList));
+            let url = '/api/forecast/raw_materials';
+            let response = await postApiData({url: url, form_data: formData, token: this.getToken()});
+            if(response.success){
+                this.rawMaterialList = response.data;
+                initTE({ Modal });
             }
         },
 
+        async getPoList(){
+            let response = await getApiData({ url: `/api/forecasts/purchase_orders`, token: this.getToken() });
+            if (response.data) {
+                this.poList = response.data;
+            }
+        },
+        itemSelectChanged() {
+            this.itemUoms = [];
+            let index = this.uomList.findIndex(uom => uom.id == this.selectedItem.base_uom_id);
+            if (index != -1) {
+                let baseUom = this.uomList[index];
+                this.itemUoms.push(baseUom);
+            }
+            index = this.uomList.findIndex(uom => uom.id == this.selectedItem.uom_id);
+            if (index != -1) {
+                let itemUom = this.uomList[index];
+                this.itemUoms.push(itemUom);
+            }
+            console.log('uom test')
+        },
+        async getUomList() {
+            let response = await getApiData({ url: `/api/uoms`, token: this.getToken() });
+            if (response.data) {
+                this.uomList = response.data;
+            }
+        },
         alertValidationMessage(field) {
             this.$notify({
                 title: `Input validation`,
@@ -557,7 +697,8 @@ export default {
 
     async created() {
         this.getMenuCategoryList();
-
+        this.getPoList();
+        this.getUomList();
     },
 
     mounted() {
