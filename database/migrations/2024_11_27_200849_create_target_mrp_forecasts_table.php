43794814e2d7e1b136d3c10be8e35373b67562f5
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2024-12-20 15:26:05 +0630

    add softdel

--- database/migrations/2024_11_27_200849_create_target_mrp_forecasts_table.php
+++ database/migrations/2024_11_27_200849_create_target_mrp_forecasts_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->integer('amount');
             $table->integer('hour')->nullable();
             $table->timestamps();
-            // $table->softDeletes();
+            $table->softDeletes();
         });
     }
 
