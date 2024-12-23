2d7ac3cabd0646b81254c13a652f0a7c89d265b8
kaungsattkhant <ookaung42@gmail.com>
2024-12-23 10:37:47 +0630

    merge:

--- database/migrations/2024_11_20_144615_create_objectivekey_staff_table.php
+++ database/migrations/2024_11_20_144615_create_objectivekey_staff_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('objectivekey_staff', function (Blueprint $table) {
+        Schema::create('objectivekey_staff', function (Blueprint $table) {  
             $table->id();
             $table->foreignId('staff_id');
             $table->foreignId('objective_key_id');
