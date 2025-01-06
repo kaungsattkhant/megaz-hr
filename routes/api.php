d5d0183eb79e84e82c2cc14d145b341e31fb2a86
kaungsattkhant <ookaung42@gmail.com>
2025-01-06 21:20:33 +0630

    merge:

--- routes/api.php
+++ routes/api.php
@@ -439,7 +439,11 @@ Route::middleware('auth:api')->group(function () {
     //     Route::post('/objectives/images', 'storeImages');
     //     Route::post('/objectives/images/{objKeyImgId}', 'updateImages');
     // });
-
+    Route::controller(ItemAPIController::class)->group(function () {
+        Route::get('item_price_list_by_item/{item_id}', 'getItemPriceListByItem');
+        Route::get('brand_by_supplier', 'brandBySupplier');
+        Route::get('supplier_by_item/{item_id}', 'supplierByItem');
+    });
 
 });
 Route::get('/features', [FeatureAPIController::class, 'getFeatureData']);
@@ -536,11 +540,7 @@ Route::post('/entities', [EntityAPIController::class, 'createEntity']);
 Route::put('/entities/{id}', [EntityAPIController::class, 'updateEntity']);
 Route::delete('/entities/{id}', [EntityAPIController::class, 'deleteEntity']);
 
-Route::controller(ItemAPIController::class)->group(function () {
-    Route::get('item_price_list_by_item/{item_id}', 'getItemPriceListByItem');
-    Route::get('brand_by_supplier', 'brandBySupplier');
-    Route::get('supplier_by_item/{item_id}', 'supplierByItem');
-});
+
 Route::get('/items', [ItemAPIController::class, 'getItemData']);
 Route::post('/items', [ItemAPIController::class, 'createItem']);
 Route::put('/items/{id}', [ItemAPIController::class, 'updateItem']);
