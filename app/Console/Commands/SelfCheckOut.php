3c6351ad8cf72e5a182d66f666e0a3c120cd24a7
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2025-01-08 17:11:30 +0630

    add self_checkout_schedule

--- app/Console/Commands/SelfCheckOut.php
+++ app/Console/Commands/SelfCheckOut.php
@@ -2,8 +2,11 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
+use App\Models\CheckIn;
 use App\Models\TimeShift;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class SelfCheckOut extends Command
 {
@@ -19,10 +22,36 @@ class SelfCheckOut extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Self-check out for staffs who have not checked out after their shift ends.';
 
     /**
      * Execute the console command.
      */
-    public function handle() {}
+    public function handle()
+    {
+        DB::beginTransaction();
+
+        try {
+            $checkIns = CheckIn::whereNull('check_out_date_time')->where('is_current_checked_in', true)
+                ->whereHas('timeShift', function ($query) {
+                    $query->where('to_time', '<', now()->format('H:i:s'));
+                })->get();
+
+            foreach ($checkIns as $checkIn) {
+                $checkOutTime = Carbon::parse($checkIn->timeShift->to_time)->addMinutes(60);
+
+                $checkIn->update([
+                    'check_out_date_time' => $checkOutTime,
+                    'is_current_checked_in' => false,
+                    'is_self_checkout' => false,
+                ]);
+            }
+
+            DB::commit();
+            $this->info('Self check-out process executed successfully.');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            $this->error('Failed to self check out: ' . $e->getMessage());
+        }
+    }
 }
