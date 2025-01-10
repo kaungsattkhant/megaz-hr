5be5e7499826e52914aa1b0092e9d91c4a7e7c6c
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2025-01-10 10:12:37 +0630

    fix

--- app/Http/Action/Transaction/PurchaseOrderTransaction.php
+++ app/Http/Action/Transaction/PurchaseOrderTransaction.php
@@ -14,10 +14,10 @@ class PurchaseOrderTransaction
 {
     public function createTransaction($model, $transactionable_type = null, $cash_account_id)
     {
-        $purchaseOrderItemGroupedByCategory=$this->groupedByCategoryAndSupplier($model);
-       
-        $purchaseOrderItemGroupedBySupplier=$this->groupedBySupplier($model);
-        
+        $purchaseOrderItemGroupedByCategory = $this->groupedByCategoryAndSupplier($model);
+
+        $purchaseOrderItemGroupedBySupplier = $this->groupedBySupplier($model);
+
         // dd($purchaseOrderItemGroupedByCategory);
         $data['date'] = now();
         $data['created_by'] = UserData()->id;
@@ -64,17 +64,17 @@ class PurchaseOrderTransaction
                 }
             }
             #credit
-           c
         }
-        $this->storeAP($purchaseOrderItemGroupedBySupplier,$cash_account_id);
+        $this->storeAP($purchaseOrderItemGroupedBySupplier, $cash_account_id);
         return $purchaseOrderItemGroupedByCategory;
     }
 
-    public function createCreditorTransaction($model,$transactionable_type=null){
-        $purchaseOrderItemGroupedByCategory=$this->groupedByCategoryAndSupplier($model);
-       
-        $purchaseOrderItemGroupedBySupplier=$this->groupedBySupplier($model);
-        
+    public function createCreditorTransaction($model, $transactionable_type = null)
+    {
+        $purchaseOrderItemGroupedByCategory = $this->groupedByCategoryAndSupplier($model);
+
+        $purchaseOrderItemGroupedBySupplier = $this->groupedBySupplier($model);
+
         // dd($purchaseOrderItemGroupedByCategory);
         $data['date'] = now();
         $data['created_by'] = UserData()->id;
@@ -122,85 +122,89 @@ class PurchaseOrderTransaction
             }
             #store AP 
             // if ($po_category->total_invoice_amount < $po_category->total_amount) {
-                // dd($po_category->total_amount - $po_category->total_invoice_amount);
-                $AP = (new StoreTransactionLedger())->storeLedger([
-                    'date' => now(),
-                    'value' => $po_category->total_amount ,
-                    'transaction_id' => $transaction->id,
-                    'account_id' => $po_category->creditor_account_id,
-                    'personable_id' => $po_category->supplier_id,
-                    'personable_type' => 'supplier',
-                    'action' => 'credit',
-                ]);
+            // dd($po_category->total_amount - $po_category->total_invoice_amount);
+            $AP = (new StoreTransactionLedger())->storeLedger([
+                'date' => now(),
+                'value' => $po_category->total_amount,
+                'transaction_id' => $transaction->id,
+                'account_id' => $po_category->creditor_account_id,
+                'personable_id' => $po_category->supplier_id,
+                'personable_type' => 'supplier',
+                'action' => 'credit',
+            ]);
             // }
         }
         $this->storeCreditor($purchaseOrderItemGroupedBySupplier);
         return $purchaseOrderItemGroupedByCategory;
     }
 
-    public function groupedByCategoryAndSupplier($model){
+    public function groupedByCategoryAndSupplier($model)
+    {
         return PurchaseOrderItem::join('po_grns', 'purchase_order_items.id', '=', 'po_grns.purchase_order_item_id')
-        ->join('items', 'purchase_order_items.item_id', '=', 'items.id')
-        ->join('categories', 'items.category_id', '=', 'categories.id')
-        ->join('suppliers', 'suppliers.id', '=', 'po_grns.supplier_id')
-        ->select(
-            'suppliers.account_id',
-            'suppliers.creditor_account_id',
-            'suppliers.id as supplier_id',
-            'suppliers.name as supplier_name',
-            'categories.name as category_name',
-            'items.category_id',
-            DB::raw('SUM(purchase_order_items.quantity * purchase_order_items.amount) as total_amount'),
-            DB::raw('SUM(po_grns.invoice_amount) as total_invoice_amount')
-        )
-        ->where('purchase_order_items.purchase_order_id', $model->id)
-        ->groupBy('suppliers.id', 'suppliers.name', 'categories.id', 'categories.name')
-        ->get();
+            ->join('items', 'purchase_order_items.item_id', '=', 'items.id')
+            ->join('categories', 'items.category_id', '=', 'categories.id')
+            ->join('suppliers', 'suppliers.id', '=', 'po_grns.supplier_id')
+            ->select(
+                'suppliers.account_id',
+                'suppliers.creditor_account_id',
+                'suppliers.id as supplier_id',
+                'suppliers.name as supplier_name',
+                'categories.name as category_name',
+                'items.category_id',
+                DB::raw('SUM(purchase_order_items.quantity * purchase_order_items.amount) as total_amount'),
+                DB::raw('SUM(po_grns.invoice_amount) as total_invoice_amount')
+            )
+            ->where('purchase_order_items.purchase_order_id', $model->id)
+            ->groupBy('suppliers.id', 'suppliers.name', 'categories.id', 'categories.name')
+            ->get();
     }
-    public function groupedBySupplier($model){
+    public function groupedBySupplier($model)
+    {
         return PurchaseOrderItem::join('po_grns', 'purchase_order_items.id', '=', 'po_grns.purchase_order_item_id')
-        ->join('items', 'purchase_order_items.item_id', '=', 'items.id')
-        ->join('categories', 'items.category_id', '=', 'categories.id')
-        ->join('suppliers', 'suppliers.id', '=', 'po_grns.supplier_id')
-        ->select(
-            'suppliers.account_id',
-            'suppliers.id as supplier_id',
-            'suppliers.creditor_account_id',
-            'suppliers.name as supplier_name',
-            DB::raw('SUM(purchase_order_items.quantity * purchase_order_items.amount) as total_amount'),
-            DB::raw('SUM(po_grns.invoice_amount) as total_invoice_amount')
-        )
-        ->where('purchase_order_items.purchase_order_id', $model->id)
-        ->groupBy('suppliers.id', 'suppliers.account_id', 'suppliers.name')
-        ->get();
+            ->join('items', 'purchase_order_items.item_id', '=', 'items.id')
+            ->join('categories', 'items.category_id', '=', 'categories.id')
+            ->join('suppliers', 'suppliers.id', '=', 'po_grns.supplier_id')
+            ->select(
+                'suppliers.account_id',
+                'suppliers.id as supplier_id',
+                'suppliers.creditor_account_id',
+                'suppliers.name as supplier_name',
+                DB::raw('SUM(purchase_order_items.quantity * purchase_order_items.amount) as total_amount'),
+                DB::raw('SUM(po_grns.invoice_amount) as total_invoice_amount')
+            )
+            ->where('purchase_order_items.purchase_order_id', $model->id)
+            ->groupBy('suppliers.id', 'suppliers.account_id', 'suppliers.name')
+            ->get();
     }
 
-    public function storeAP($accountPayables,$cash_account_id){
-        foreach($accountPayables as $accountPayable){
+    public function storeAP($accountPayables, $cash_account_id)
+    {
+        foreach ($accountPayables as $accountPayable) {
             if ($accountPayable->total_invoice_amount < $accountPayable->total_amount) {
-                $accountPayable=AccountPayable::create([
-                    'type'=>'addition',
-                    'date_time'=>now(),
-                    'amount'=>$accountPayable->total_amount - $accountPayable->total_invoice_amount,
-                    'supplier_id'=>$accountPayable->supplier_id,
-                    'account_id'=>$accountPayable->account_id,
-                    'cash_account_id'=>$cash_account_id,
-                    'created_by'=>UserData()->id,
+                $accountPayable = AccountPayable::create([
+                    'type' => 'addition',
+                    'date_time' => now(),
+                    'amount' => $accountPayable->total_amount - $accountPayable->total_invoice_amount,
+                    'supplier_id' => $accountPayable->supplier_id,
+                    'account_id' => $accountPayable->account_id,
+                    'cash_account_id' => $cash_account_id,
+                    'created_by' => UserData()->id,
                 ]);
             }
         }
     }
 
-    public function storeCreditor($creditors){
-        foreach($creditors as $creditor){
-                $creditorBalance=CreditorBalance::create([
-                    'type'=>'addition',
-                    'date_time'=>now(),
-                    'amount'=>$creditor->total_amount ,
-                    'supplier_id'=>$creditor->supplier_id,
-                    'account_id'=>$creditor->creditor_account_id,
-                    'created_by'=>UserData()->id,
-                ]);
+    public function storeCreditor($creditors)
+    {
+        foreach ($creditors as $creditor) {
+            $creditorBalance = CreditorBalance::create([
+                'type' => 'addition',
+                'date_time' => now(),
+                'amount' => $creditor->total_amount,
+                'supplier_id' => $creditor->supplier_id,
+                'account_id' => $creditor->creditor_account_id,
+                'created_by' => UserData()->id,
+            ]);
         }
     }
     public function storeLedger($data)
