0355647cc3b2a6e6375bac8e22f916d86d543d5a
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2025-01-20 10:57:16 +0630

    add

--- app/Repositories/Item/ItemRepositoryInterface.php
+++ app/Repositories/Item/ItemRepositoryInterface.php
@@ -22,7 +22,7 @@ interface ItemRepositoryInterface
 
     public function supplierByItem($itemId);
 
-    public function brandBySupplier($request, $itemId, $supplierId);
+    public function brandBySupplier($request);
 
     public function itemImport($request);
 }
