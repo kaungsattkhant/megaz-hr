200993026cb4d42da664b2df245fe658ee5da413
Phyo Kyaw San <phyojupiter9@gmail.com>
2025-01-22 17:07:32 +0630

    Lead time list in progress

---
+++ resources/js/Components/Supplier/SupplierLeadTimeComponent.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <p class=" text-lg font-semibold font-inter">
+            Supplier: {{ supplier.name }}
+        </p>
+    </div>
+    <div class="mt-4 bg-white">
+        <div class="btn-container">
+            <div class=" flex gap-x-4">
+                <label for="search" class="search-input">
+                    <input type="text" class="input-search" placeholder="Search" v-model="searchInput">
+                    <i class="fal fa-search"></i>
+                </label>
+                <!-- <button class="add-btn h-8 text-[13px] font-inter" @click="searchBtnClicked()">Search</button>
+                <button class="add-btn h-8 text-[13px] font-inter" @click="clearSearchBtnClicked()">Clear</button> -->
+            </div>
+            <div class="flex justify-end flex-col gap-y-4">
+                <div>
+                    <a href="/suppliers/create" class="add-btn ">
+                        Add New
+                    </a>
+                </div>
+
+                <div>
+                    <p> Average Lead Time: 30min </p>
+                </div>
+            </div>
+        </div>
+        <div class="box-container-table">
+            <div class="overflow-x-auto">
+                <div class=" table-container ">
+                    <table class="primary-table">
+                        <thead class="">
+                            <tr>
+                                <th scope="col" class=" ">
+                                    #
+                                </th>
+                                <th scope="col" class=" ">
+                                    Item
+                                </th>
+                                <th scope="col" class=" ">
+                                    Accepted Lead Time
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <div class="contents" v-for="(leadTime, index) in leadTimes" :key="index">
+                                <tr class="">
+                                    <td class="  font-medium ">
+                                        {{ index + 1 }}
+                                    </td>
+                                    <td class="whitespace>nowrap  ">
+                                        {{ leadTime.item_name }}
+                                    </td>
+                                    <td class="whitespace>nowrap  ">
+                                        {{ leadTime.item_name }}
+                                    </td>
+                                </tr>
+                            </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Modal, Ripple, initTE, Input } from "tw-elements";
+import { mapGetters } from "vuex";
+import { getApiData, deleteApiData } from '../../utilities/ajax-helpers';
+
+export default {
+    props: ['supplier'],
+    data() {
+        return {
+            leadTimes: [],
+
+            searchInput:null,
+
+            currentPage: 0,
+            perPage: 0,
+            lastPage: 0,
+            totalData: 0,
+
+            averageLeadTime: null,
+            averageOrderTime: null,
+        };
+    },
+
+    methods: {
+        ...mapGetters(['getToken']),
+
+        async searchBtnClicked() {
+            this.getSupplierList(1);
+        },
+
+        clearSearchBtnClicked() {
+            this.searchInput = null;
+            this.getSupplierList(1);
+        },
+
+        async getLeadTimes(){
+            let response = await getApiData({ url: `/api/supplier_lead_time/${this.supplier.id}`, token: this.getToken() });
+            if (response.data) {
+                this.leadTimes = response.data;
+            }
+        },
+    },
+
+    created() {
+        this.getLeadTimes();
+    },
+
+    mounted() {
+        initTE({Modal, Ripple, Input});
+    }
+}
+</script>
