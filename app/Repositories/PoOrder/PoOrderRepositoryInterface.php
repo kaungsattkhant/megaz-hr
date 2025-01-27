325ee81d85717f096bfb4c2b975f51356ddb1a4d
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2025-01-27 17:02:03 +0630

    fix res

--- app/Repositories/PoOrder/PoOrderRepositoryInterface.php
+++ app/Repositories/PoOrder/PoOrderRepositoryInterface.php
@@ -9,7 +9,7 @@ interface PoOrderRepositoryInterface
 
   public function getPoOrderItems(Request $request);
   public function test($request);
-  public function  storePoOrderItems($validatedData);
+  public function storePoOrderItems($validatedData);
   public function getPoOrderArrivalList(Request $request);
   public function getPoOrderArrivalListByItemId($itemId);
   public function getInvoiceBySupplier($supplierId);
