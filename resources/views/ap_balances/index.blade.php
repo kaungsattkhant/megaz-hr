4151d8b094f491266abdb16f651eca5bec111a0d
Nevarnar <loverhein2011@gmail.com>
2025-01-28 13:47:52 +0630

    fix sidebar

--- resources/views/ap_balances/index.blade.php
+++ resources/views/ap_balances/index.blade.php
@@ -3,6 +3,7 @@
 @section('page_title', 'AP Balances')
 
 @section('ap_balances', 'active-link')
+@section('cash_flow_collapse', 'show')
 @section('content')
     <ap-balances-component/>
 @endsection
--- resources/views/creditor_balances/index.blade.php
+++ resources/views/creditor_balances/index.blade.php
@@ -3,6 +3,7 @@
 @section('page_title', 'Creditor Balances')
 
 @section('creditor_balances', 'active-link')
+@section('cash_flow_collapse', 'show')
 @section('content')
     <creditor-balances-component/>
 @endsection
