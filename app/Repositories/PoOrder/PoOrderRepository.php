437cc413a0d823bf94351e71763ce119e686a0e0
kaungsattkhant <ookaung42@gmail.com>
2025-01-30 10:49:07 +0630

    latest push

--- app/Repositories/PoOrder/PoOrderRepository.php
+++ app/Repositories/PoOrder/PoOrderRepository.php
@@ -343,6 +343,7 @@ class PoOrderRepository implements PoOrderRepositoryInterface
 
   public function storePoOrderItems($validatedData)
   {
+    dd($validatedData);
     try {
       $validatedData['created_by'] = UserData()->id;
       $poOrder = PoOrder::create($validatedData);
