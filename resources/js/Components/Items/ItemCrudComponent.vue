aa237a58843a442208ba3e3fbd777fb786c951dd
Nevarnar <loverhein2011@gmail.com>
2025-02-04 12:19:59 +0630

    fix

--- resources/js/Components/Items/ItemCrudComponent.vue
+++ resources/js/Components/Items/ItemCrudComponent.vue
@@ -27,8 +27,12 @@
                 <button class="add-btn h-8 mx-2 " @click="clearSearchBtnClicked">Clear</button>
 
             </div>
-            <div class="flex justify-end flex-col">
-
+            <div class="flex justify-end gap-x-4">
+                <button type="button"
+                    class="add-btn transition duration-150 ease-in-out focus:outline-none focus:ring-0 "
+                    data-te-toggle="modal" data-te-target="#import_modal">
+                    Excel Import
+                </button>
                 <button type="button"
                     class="add-btn transition duration-150 ease-in-out focus:outline-none focus:ring-0 "
                     data-te-toggle="modal" data-te-target="#create_modal">
@@ -358,6 +362,54 @@
         </div>
     </div>
 
+    <!-- import Modal -->
+    <div data-te-modal-init
+        class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+        id="import_modal" tabindex="-1" aria-labelledby="import_modalLabel" aria-hidden="true">
+        <div data-te-modal-dialog-ref
+            class="pointer-events-none relative w-auto mb-12 translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
+            <div
+                class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none">
+
+                <div class="relative flex justify-between py-2 px-6 border-b">
+                    <!--Modal title-->
+                    <h5 class="text-base text-center mt-2 font-semibold leading-normal font-inter"
+                        id="import_modalLabel">
+                        Import Excel
+                    </h5>
+                    <!--Close button-->
+                    <button type="button" class="text-xs focus:shadow-none focus:outline-none" data-te-modal-dismiss
+                        aria-label="Close">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="h-4 w-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+
+                <!--Modal body-->
+                <div class="relative px-6 py-4 border-b" data-te-modal-body-ref>
+                    <div class="mb-4">
+                        <label for="" class="label-form mb-3">
+                            Excel
+                        </label>
+                        <input type="file" placeholder="Excel" class="input-ui">
+                    </div>
+                </div>
+                <!--Modal footer-->
+                <div class="flex justify-end gap-x-4 px-6 mb-6 pt-4">
+                    <button type="button" class="cancel-btn focus:shadow-none focus:outline-none" data-te-modal-dismiss
+                        aria-label="Close">
+                        Cancel
+                    </button>
+                    <button type="button" class="add-btn focus:outline-none focus:ring-0 " @click="importBtnClicked()">
+                        Create
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Modal -->
     <div data-te-modal-init
         class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
@@ -448,6 +500,7 @@ export default {
 
             // brandsList: [],
             // selectedBrands: [],
+            selectedFile:null,
         };
     },
 
@@ -594,6 +647,28 @@ export default {
             this.searchInput = null;
             this.searchCategory = null;
             this.getItemList(1);
+        },
+        handleFileChange(event) {
+            const selectedFile = event.target.files[0];
+            this.selectedFile = selectedFile;
+        },
+        async importBtnClicked() {
+            let formData = new FormData();
+            formData.append('image', this.selectedFile);
+            let response = await postApiData({ url: '/api/import_account', form_data: formData, token: this.getToken() });
+            if (response.success) {
+                this.$notify({
+                    text: `Excel Imported successfully`,
+                    type: "info"
+                });
+                this.selectedFile = null;
+            }
+            else {
+                this.$notify({
+                    text: `Excel Imported failed`,
+                    type: "error"
+                });
+            }
         },
     },
 
