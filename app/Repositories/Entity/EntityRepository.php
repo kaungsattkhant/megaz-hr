aa15843168dc2ebd7a5e61d9c2dab709db97f8cd
kaungsattkhant <ookaung42@gmail.com>
2025-02-11 14:42:54 +0630

    done

--- app/Repositories/Entity/EntityRepository.php
+++ app/Repositories/Entity/EntityRepository.php
@@ -238,6 +238,9 @@ class EntityRepository implements EntityRepositoryInterface
         if ($entity->entity_type == 'room') {
             $activeInvoiceSession = InvoiceSession::where('is_active', 1)->where('entity_id', $entityId)->first();
             $invoice = $activeInvoiceSession->invoice;
+            if(!$invoice){
+                ResponseMessage('Active Invoice Not Found',419);
+            }
             // $entitySession = EntitySession::where('is_active', 1)
             //     ->with(['entity', 'roomSessions', 'roomSession.invoice'])
             //     ->where('entity_id', $entityId)->first();
