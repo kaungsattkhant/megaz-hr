2abc62de831d684e5bde347e42a97cc731d9eea2
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2025-02-17 11:08:12 +0630

    fix resources

--- app/Http/Resources/NotificationableResource.php
+++ app/Http/Resources/NotificationableResource.php
@@ -19,26 +19,26 @@ class NotificationableResource extends JsonResource
             'id' => $this->id,
             'title' => $this->title,
             'date_time' => $this->date_time,
-            'from_date' => $this->from_date,
-            'to_date' => $this->to_date,
-            'place' => $this->place,
-            'description' => $this->description,
-            'type' => $this->notificationable_type,
+            'from_date' => $this->from_date ?? null,
+            'to_date' => $this->to_date ?? null,
+            'place' => $this->place || null,
+            'description' => $this->description ?? null,
+            'type' => $this->notification->notificationable_type ?? null,
+            'warning_type' => $this->warning_type ?? null,
+            'meeting_type' => $this->meeting_type ?? null,
+            'org_news_type' => $this->org_news_type ?? null,
+            'training_type' => $this->training ?? null,
             'participants' => ParticipantResource::collection($this->participants),
         ];
 
-        if ($this->notificationable_type === "meeting") {
-            $notificationable['chaired_by_name'] = optional($this->chaired_by)->name;
-            $notificationable['chaired_by_id'] = optional($this->chaired_by)->id;
+        if ($this->notification->notificationable_type === "meeting") {
+            $notificationable['chaired_by_id'] = optional($this->chairedBy)->id;
+            $notificationable['chaired_by_name'] = optional($this->chairedBy)->name;
         }
-
-        // Check if the notificationable type is 'training' and add the 'trained_by' name
-        if ($this->notificationable_type === "training") {
-            $notificationable['trained_by_name'] = optional($this->trained_by)->name;
-            $notificationable['trained_by_id'] = optional($this->trained_by)->id;
+        if ($this->notification->notificationable_type === "training") {
+            $notificationable['trained_by_id'] = optional($this->trainedBy)->id;
+            $notificationable['trained_by_name'] = optional($this->trainedBy)->name;
         }
-
-
         return $notificationable;
     }
 }
