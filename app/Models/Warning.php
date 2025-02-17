c4919d79d8267b4fc7185c6f9bf2832d48ff732d
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2025-02-17 21:38:09 +0630

    add type

--- app/Models/Warning.php
+++ app/Models/Warning.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -17,6 +18,7 @@ class Warning extends Model
         'description',
         'created_by',
         'warning_type',
+        'type_id'
     ];
 
     protected $hidden = ['created_at', 'updated_at'];
@@ -26,9 +28,9 @@ class Warning extends Model
         return $this->belongsTo(Staff::class, 'created_by');
     }
 
-    public function type(): morphMany
+    public function type(): BelongsTo
     {
-        return $this->morphMany(Type::class, 'typeable');
+        return $this->belongsTo(Type::class);
     }
 
     public function participants(): MorphMany
