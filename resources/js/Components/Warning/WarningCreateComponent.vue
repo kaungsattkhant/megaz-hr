55d54ee54fd22b879df052b1ae8cb0e4aff41d17
Nevarnar <loverhein2011@gmail.com>
2025-02-20 17:46:46 +0630

    fix

--- resources/js/Components/Warning/WarningCreateComponent.vue
+++ resources/js/Components/Warning/WarningCreateComponent.vue
@@ -143,7 +143,7 @@
             </div>
         </div>
         <div class="flex gap-x-4">
-            <a href="/org_news" class="cancel-btn focus:shadow-none focus:outline-none ">
+            <a href="/warning" class="cancel-btn focus:shadow-none focus:outline-none ">
                 Cancel
             </a>
             <button class="add-btn" @click="createBtnClicked">
