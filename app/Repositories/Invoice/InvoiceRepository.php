f9153882fe00452b543802c233b49118214f4bbb
kaungsattkhant <ookaung42@gmail.com>
2025-02-25 16:31:20 +0630

    check null

--- app/Repositories/Invoice/InvoiceRepository.php
+++ app/Repositories/Invoice/InvoiceRepository.php
@@ -130,6 +130,7 @@ class InvoiceRepository implements InvoiceRepositoryInterface
     {
         DB::beginTransaction();
         try {
+            dd($data['room_discount_id']);
             $roomDiscountId = $data['room_discount_id'];
             $roomDiscount = null;
             if ($roomDiscountId) {
