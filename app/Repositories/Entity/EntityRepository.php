d872f7255e560a5663c3cf2fa09b74c7cb73346b
kaungsattkhant <ookaung42@gmail.com>
2025-03-02 22:09:20 +0630

    merge:feature

--- app/Repositories/Entity/EntityRepository.php
+++ app/Repositories/Entity/EntityRepository.php
@@ -93,13 +93,14 @@ class EntityRepository implements EntityRepositoryInterface
         foreach ($entities as $entity) {
             if ($entity->entity_type == 'room' && $entity->is_active == 1) {
                 $invoiceSession = InvoiceSession::where('is_active', 1)->where('entity_id', $entity->id)->first();
-
                 if ($invoiceSession) {
                     $entity->start_time = $invoiceSession->start_date_time;
                     $entity->end_time = $invoiceSession->end_date_time;
+                    $entity->invoice=$invoiceSession;
                 } else {
                     $entity->start_time = null;
                     $entity->end_time = null;
+                    $entity->invoice=null;
                 }
             }
             //     $roomSessions = collect();
