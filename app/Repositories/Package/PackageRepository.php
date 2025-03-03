f75a877a7da4cbb8f2cd8ccb54d1c4c3995f6374
kaungsattkhant <ookaung42@gmail.com>
2025-03-03 23:34:45 +0630

    done

--- app/Repositories/Package/PackageRepository.php
+++ app/Repositories/Package/PackageRepository.php
@@ -27,6 +27,8 @@ class PackageRepository implements PackageRepositoryInterface
             ->where('to_date', '>=', $validateDate)
             ->with([
                 'menuPackages.menu.prices',
+                'rooms',
+                'accessories',
                 'menuPackages.menu.menuServiceDiscounts' => function ($query) use ($validateDate) {
                     $query->where('from_date', '<=', $validateDate)
                         ->where('to_date', '>=', $validateDate)
