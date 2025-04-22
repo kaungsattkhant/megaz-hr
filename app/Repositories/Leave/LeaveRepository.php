6ce5db5fc0396d5de71ad739683e4855b6003b85
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2025-04-22 10:45:28 +0630

    modify exit pass list

--- app/Repositories/Leave/LeaveRepository.php
+++ app/Repositories/Leave/LeaveRepository.php
@@ -402,7 +402,6 @@ class LeaveRepository implements LeaveRepositoryInterface
     $staff = Staff::where('id', $staffId)->firstOrFail();
     $exitPasses = ExitPass::where('staff_id', $staff->id)
       ->with(['exitCategory'])
-      ->select('id', 'exit_category_id', 'staff_id', 'exit_date_time', 'arrival_date_time', 'status')
       ->orderByDesc('id')
       ->paginate(config('common.list_count'));
     ResponseData($exitPasses);
