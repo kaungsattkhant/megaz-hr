7d57a2953bd93ec56e23de36edd17c8f7e1027ee
kaungsattkhant <ookaung42@gmail.com>
2025-05-27 15:23:44 +0630

    done

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -213,7 +213,7 @@ class OrderService
             $cancelledMenu = array_filter($data['menuArray'], function ($menu) {
                 return isset($menu['is_package']) && in_array($menu['is_package'], [-1]);
             });
-            $total=0;
+            $detaultQuantity=1;
             foreach ($filteredMenu as $menuData) {
                 // if($invoice->invoice_type=='package' && $menuData['is_package']=-1){}
                 $menu = Menu::find($menuData['menu_id']);
@@ -278,7 +278,6 @@ class OrderService
                         $totalDiscount += $discountAmount;
                     }
                 }
-                // dd($order);
                 if ($order) {
                     // $order->total_quantity += $menuData['quantity'];
                     // $order->total_discount_price += $discountAmount; // update total discount only for this order
@@ -304,16 +303,24 @@ class OrderService
                         ->first();
                     $menuData['date'] = CurrentTime();
                     $menuData['order_id'] = $order->id;
-                    $menuData['price'] = $menuData['original_price'];
+                    $menuData['quantity'] = $defaultQuantity;
+                    // $menuData['price'] = $menuData['original_price'];
                     $menuData['status'] = 'pos_confirmed';
                     $menuData['area_id'] = $cookingAreaId;
-                    $menuData['sub_total_price'] = ($menuData['original_price']) - $defaultDiscountAmont; //after  
+                    $menuData['sub_total_price'] = (isset($menuData['is_package']) && $menuData['is_package'])
+                        ? 0
+                        : ($menuData['original_price']) - $defaultDiscountAmont; //after  
+                    $menuData['price'] = (isset($menuData['is_package']) && $menuData['is_package'])
+                        ? 0
+                        : $menuData['original_price']; //after
+                    // $menuData['sub_total_price'] = ($menuData['original_price']) - $defaultDiscountAmont; //after  
                     $menuData['discount_value'] = $defaultDiscountAmont;
                     // $order_items = OrderItem::create($menuData);
                     // $orderItems = OrderItem::find($order_items->id);
                     // $orderItems->menu = $orderItems->menu;
                     // $orderItemsArray[] = $orderItems;
                 } else {
+                    // dd($menuData);
                     $orderData['invoice_id'] = $invoiceId;
                     $orderData['date'] = CurrentTime();
                     $orderData['total'] = (isset($menuData['is_package']) && $menuData['is_package'])
@@ -361,6 +368,7 @@ class OrderService
                 $insertData = [];
                 for ($i = 0; $i < (int) $quantityCount; $i++) {
                     // $insertData[] = $orderItemData;
+                    $menuData['quantity']=$defaultQuantity;
                     $order_item = OrderItem::create($menuData);
                     $insertData[] = $order_item;
                     if ($order_item->is_foc == 1) {
