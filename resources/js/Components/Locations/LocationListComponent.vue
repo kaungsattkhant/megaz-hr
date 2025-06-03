df77de16e5c832ae53dd9a161523fcdda10ae090
Nevarnar <loverhein2011@gmail.com>
2025-06-03 11:02:14 +0630

    fix

--- resources/js/Components/Locations/LocationListComponent.vue
+++ resources/js/Components/Locations/LocationListComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <p class=" text-lg font-semibold font-inter">
-            Resignations
+            Locations
         </p>
     </div>
     <div class="mt-4 bg-white">
