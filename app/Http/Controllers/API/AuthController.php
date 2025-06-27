423e54d7a9acb9d924b6e5eea4c58f4b33161aab
Phyo Kyaw San <phyojupiter9@gmail.com>
2025-06-27 22:52:36 +0630

    Fix auth

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
         $loginResponse = (new APILoginAction("phone_number", $request->phone_number, $request->password, "App\Models\Staff"))->run("staff_token");
 
         if($loginResponse["code"] != 200){
-            ResponseMessage($loginResponse["message"], 401);
+            ResponseMessage($loginResponse["message"], $loginResponse["code"]);
         }
         else{
             $staff = Staff::with(["gender","department","roles"])->find($loginResponse["user"]["id"]);
