02ab9fbcc5235c14b78a366153a25b6d3b3b2d03
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2025-06-27 20:05:36 +0630

    add jd

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\JobSpecification;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Skill extends Model
 {
@@ -23,8 +25,13 @@ class Skill extends Model
         return $this->morphMany(AvailableCookingPlace::class, 'cooking_placeable');
     }
 
-        public function staffs()
-        {
-            return $this->belongsToMany(Staff::class,'skill_staff');
-        }
+    public function staffs()
+    {
+        return $this->belongsToMany(Staff::class,'skill_staff');
+    }
+
+    public function jobSpecifications(): BelongsToMany
+    {
+        return $this->belongsToMany(JobSpecification::class, 'job_specification_skill','skill_id','job_specification_id');
+    }
 }
