17e9e9e340c40ccd5a715854f46104711befc911
Phyo Kyaw San <phyojupiter9@gmail.com>
2025-06-28 10:38:42 +0630

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,7 @@
 
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 
-	<title> test</title>
+	<title> @yield('page_title')</title>
     @vite('resources/js/app.js')
 
     <link rel="stylesheet" href="{{asset('css/style_web.css')}}">
@@ -88,7 +88,7 @@
         $(window).on('load', function() {
             scrollToActiveLink();
         });
-        
+
     });
 </script>
 <script>
