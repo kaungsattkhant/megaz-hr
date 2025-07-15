d56f3d0c8d8735a22f22769aa11ae62ef7e9e1de
kaungsattkhant <ookaung42@gmail.com>
2025-07-15 19:26:52 +0630

    merge:ui/

--- app/Repositories/CashBook/CashBookRepository.php
+++ app/Repositories/CashBook/CashBookRepository.php
@@ -32,7 +32,7 @@ class CashBookRepository implements CashBookInterface
                 // dd($transaction->transactionable);
             }
             foreach ($transaction->ledgers as $ledger) {
-                if (in_array(config('common.pos_cash'), $cashAccountId) || in_array(config('common.pos_cash'), $cashAccountId)) {
+                if (in_array($ledger->account_id, $cashAccountId) || in_array($ledger->account_id, $cashAccountId)) {
                     $ledger->action == 'debit' ? $current_debit_amount += $transaction->amount : $current_credit_amount += $transaction->amount;
                     $transaction->title = $transaction->transactionable_type == 'invoice' ? $ledger->account->name . '(' . $transaction->transactionable->invoice_id . ')' : $ledger->account->name;
                     $transaction->type = $ledger->account->name;
