e286bd56491b8e019430b9206e2f9fd749df1bcd
Nevarnar <loverhein2011@gmail.com>
2025-07-23 15:58:01 +0630

    pos sale report

---
+++ resources/js/Components/Pos/SaleReports/PosSaleReportComponent.vue
@@ -0,0 +1,250 @@
+<template>
+    <div>
+        <div class="">
+            <div class="w-full pt-9 px-6">
+                <div class="flex justify-between mb-4">
+                    <div class="flex gap-x-3">
+                        <div class=" flex gap-x-4">
+                            <input type="date" class="h-8 mr-2 rounded-md" v-model="selectedDate" @change="dateChanged()">
+                            <!-- <input type="date" class="h-8 mx-2 rounded-md" v-model="toDate" @change="dateChanged()"> -->
+                        </div>
+                        
+                    </div>
+                    <div class="flex gap-x-3">
+                        <select name="" id="" v-model="selectedArea"  placeholder="Select Area"
+                            class="text-sm border border-gray-300 input-ui w-full !bg-white rounded-lg focus:ring-0">
+                            <option :value="area" v-for="area in areaList"> {{ area.name }} </option>
+                        </select>
+                        <!-- <button class="pos-add-btn" data-te-toggle="modal" data-te-target="#create_cashbook_modal">
+
+                            Add
+
+                        </button> -->
+                    </div>
+                </div>
+
+                <div>
+                    <div class="bg-white px-4">
+                        <table class="min-w-full text-left text-sm font-light">
+                            <thead class="border-b font-medium">
+                                <tr>
+                                    <th scope="col" class="px-6 py-4">Id</th>
+                                    <th scope="col" class="px-6 py-4">Menu Name</th>
+                                    <th scope="col" class="px-6 py-4">Count</th>
+                                    <th scope="col" class="px-6 py-4">Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="" v-for="(item,index) in primaryList">
+                                    <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                        {{ index+1 }}
+                                    </td>
+                                    
+                                    <td class="whitespace-nowrap px-6 py-4">
+                                        {{ item.menu.name }}
+                                    </td>
+                                    <td class="whitespace-nowrap px-6 py-4">
+                                        {{ item.total_quantity }}
+                                    </td>
+                                    <td class="whitespace-nowrap px-6 py-4">
+                                        {{ (item.total_amount).toLocaleString() }}
+                                    </td>
+                                </tr>
+                                <tr class="">
+                                    <td></td>
+                                    <td class="whitespace-nowrap px-6 py-4">
+                                        {{ total_quantity }}
+                                    </td>
+                                    <td class="whitespace-nowrap px-6 py-4">
+                                        {{ total_amount.toLocaleString() }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <!-- <div data-te-modal-init
+            class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+            id="create_cashbook_modal" tabindex="-1" aria-labelledby="createCashbookModalLabel" aria-modal="true"
+            role="dialog">
+            <div data-te-modal-dialog-ref
+                class="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]">
+                <div
+                    class="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none">
+                    <div class="relative  p-4">
+                        <p class="text-xl w-full text-center">
+                            Create Cashbook
+                        </p>
+                        <button type="button" class="absolute top-4 right-4 focus:shadow-none focus:outline-none
+                        " id="closeModal" data-te-modal-dismiss aria-label="Close">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="h-5 w-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div class="relative px-16 py-4" data-te-modal-body-ref>
+                        <div class="mb-4">
+                            <label for="" class="block text-sm text-black mb-3">
+                                Parent Account
+                            </label>
+                            <select name="" id="" v-model="selectedSubAccount"
+                                class="text-sm border border-gray-300 input-ui w-full bg-transparent rounded-lg focus:ring-0"
+                                @change="subAccountSelectChanged">
+                                <option :value="subAccount" v-for="(subAccount, subAccountIndex) in subAccountList"
+                                    :key="subAccountIndex">
+                                    {{ subAccount.name }}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="mb-4">
+                            <label for="" class="block text-sm text-black mb-3">
+                                Title
+                            </label>
+                            <select name="" id="" v-model="selectedAccount"
+                                class="text-sm border border-gray-300 input-ui w-full bg-transparent rounded-lg focus:ring-0">
+                                <option :value="account.id" v-for="(account, accountIndex) in accountList"
+                                    :key="accountIndex">
+                                    {{ account.name }}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="mb-4">
+                            <label for="" class="block text-sm text-black mb-3">
+                                Account Type
+                            </label>
+                            <select name="" id="" v-model="accType"
+                                class="text-sm border border-gray-300 input-ui w-full bg-transparent rounded-lg focus:ring-0">
+                                <option :value="account" v-for="account in cashAccountList"> {{ account.name }}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="mb-4">
+                            <label for="" class="block text-sm text-black mb-3">
+                                Type
+                            </label>
+                            <select name="" id="" v-model="action"
+                                class="text-sm border border-gray-300 input-ui w-full bg-transparent rounded-lg focus:ring-0">
+                                <option value="debit">Debit</option>
+                                <option value="credit">Credit</option>
+                            </select>
+                        </div>
+                        <div class="mb-4">
+                            <label for="" class="block text-sm text-black mb-3">
+                                Amount
+                            </label>
+                            <input type="text" placeholder="Amount" v-model="amount"
+                                class="text-sm border border-gray-300 input-ui w-full bg-transparent rounded-lg focus:ring-0">
+                        </div>
+                        <div class="mb-4">
+                            <label for="" class="block text-sm text-black mb-3">
+                                Remark
+                            </label>
+                            <textarea v-model="description"
+                                class="text-sm border border-gray-300 input-ui w-full bg-transparent rounded-lg focus:ring-0"
+                                name="" id="" cols="30" rows="10"></textarea>
+                        </div>
+
+                    </div>
+
+                    <div class="flex justify-center px-12 mb-6">
+                        <button @click="createBtnClicked" class="pos-add-btn !px-16 focus:outline-none focus:ring-0 ">
+                            Create
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+
+
+
+    </div>
+
+</template>
+<script>
+    import { Modal, Ripple, Select, Datepicker, initTE, Input } from "tw-elements";
+    import { getApiData, postApiData, deleteApiData } from '../../../utilities/ajax-helpers';
+    import { mapGetters } from "vuex";
+
+    export default {
+        data() {
+            return {
+                primaryList:[],
+                areaList: [],
+
+                selectedDate: null,
+                selectedArea: null,
+
+                url_date: '',
+                url_area: '',
+                url: '/api/sale-reports'
+            };
+        },
+
+        methods: {
+            ...mapGetters(['getToken']),
+
+            async getPrimaryList(pageNumber){
+                let url = this.url + this.url_date + this.url_area;
+                const response = await getApiData({ url: url, token: this.getToken() });
+                if (response.data) {
+                    if(response.data.data){
+                        this.primaryList = response.data.data.data;
+                    }
+                    else{
+                        this.primaryList = response.data
+                    }
+                }
+            },
+            dateChanged(){
+                if(this.selectedDate){
+                    this.url_date = '?date=' + this.selectedDate;
+                    this.getPrimaryList();
+                }
+                
+            },
+            
+
+            async getSubAccountList() {
+                let url = `/api/sub_accounts`;
+                let response = await getApiData({ url: url, token: this.getToken() });
+                if (response.data) {
+                    this.subAccountList = response.data;
+                }
+            },
+
+            
+            // async createBtnClicked() {
+            //     let formData = new FormData();
+            //     formData.append('description', this.description);
+            //     formData.append('account_id', this.selectedAccount);
+            //     formData.append('value', this.amount);
+            //     formData.append('cash_account_id', this.accType.id);
+            //     formData.append('action', this.action);
+            //     formData.append('is_confirmed', 1);
+
+            //     let url = `/api/transactions`;
+            //     let response = await postApiData({ url: url, form_data: formData, token: this.getToken() });
+            //     if (response.success) {
+            //         window.location.reload();
+            //     }
+            // },
+
+            
+        },
+        mounted()
+        {
+            initTE({ Modal, Select, Ripple, Datepicker });
+        },
+
+        created(){
+            this.getPrimaryList();
+        }
+    }
+</script>
