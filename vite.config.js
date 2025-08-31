ceeb73fd2e9be907d7cd47efe1d030adf831cbfe
Phyo Kyaw San <phyojupiter9@gmail.com>
2025-08-31 19:00:14 +0630

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        sourcemap: process.env.NODE_ENV !== 'production'
+    },
     plugins: [
         vue(),
         laravel({
