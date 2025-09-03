ac73eb50bcb34eea761c296b66756e853e85974a
kaungsattkhant <ookaung42@gmail.com>
2025-09-03 23:08:40 +0630

    done

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -59,7 +59,7 @@ class OrderService
                 ResponseMessage('Menu Area not found', 404);
             }
             $cookingAreaId = $menuArea->cooking_area_id;
-
+            $inventoryId = $this->getInventoryIdByCookingArea($cookingAreaId);
             if (!$menu) {
                 ResponseMessage('Menu not found', 404);
             }
@@ -117,7 +117,7 @@ class OrderService
                 $orderItemData['price'] = $data['original_price']; //after  
 
                 $insertData = [];
-                $this->checkInventoryEnough($data['menu_id'], $quantityCount);
+                $this->checkInventoryEnough($data['menu_id'], $inventoryId, $quantityCount);
                 for ($i = 0; $i < (int) $quantityCount; $i++) {
                     // $insertData[] = $orderItemData;
                     $createdOrderItem = OrderItem::create($orderItemData);
@@ -177,10 +177,10 @@ class OrderService
                 $orderItemData['price'] = $data['original_price']; //after  
                 $insertData = [];
                 //check inventory
-                $this->checkInventoryEnough($data['menu_id'], $quantityCount);
+                $this->checkInventoryEnough($data['menu_id'], $inventoryId, $quantityCount);
                 if (isset($data['selling_extra_id']) && !empty($data['selling_extra_id'])) {
                     // $this->createOrderItemExtra($createdOrderItem, $sellingExtraIds);
-                    $this->checkSellingExtraInventoryIsEnough($sellingExtraIds);
+                    $this->checkSellingExtraInventoryIsEnough($sellingExtraIds, $inventoryId);
                 }
                 //end check inventory
                 for ($i = 0; $i < (int) $quantityCount; $i++) {
@@ -256,13 +256,17 @@ class OrderService
             $cookingAreaIds = [];
             foreach ($filteredMenu as $menuData) {
                 $totalExtraPrice = 0;
-                $this->checkInventoryEnough($menuData['menu_id'], $menuData['quantity']);
+                $menuData['area_id'] = $menuData['cooking_area_id'];
+                $cookingAreaId = $menuData['cooking_area_id'];
+                $inventoryId = $this->getInventoryIdByCookingArea($cookingAreaId);
+                $cookingAreaIds[] = $cookingAreaId;
+                $this->checkInventoryEnough($menuData['menu_id'], $inventoryId, $menuData['quantity']);
                 if (isset($menuData['selling_extra_id']) && !empty($menuData['selling_extra_id'])) {
                     foreach ($menuData['selling_extra_id'] as $sellingExtraId) {
                         $sellingExtra = SellingExtra::find($sellingExtraId);
                         $totalExtraPrice += $sellingExtra->price;
                     }
-                    $this->checkSellingExtraInventoryIsEnough($menuData['selling_extra_id']);
+                    $this->checkSellingExtraInventoryIsEnough($menuData['selling_extra_id'], $inventoryId, );
                 }
                 // if($invoice->invoice_type=='package' && $menuData['is_package']=-1){}
                 $menu = Menu::find($menuData['menu_id']);
@@ -277,10 +281,7 @@ class OrderService
                 if (!$menu) {
                     ResponseMessage('Menu is invalid', 419);
                 }
-                $menuData['area_id'] = $menuData['cooking_area_id'];
-                $cookingAreaId = $menuData['cooking_area_id'];
-                $inventoryId = $this->getInventoryIdByCookingArea($cookingAreaId);
-                $cookingAreaIds[] = $cookingAreaId;
+
                 // $menuCategoryArea = MenuCategoryArea::where('menu_category_id', $menu->menu_category_id)
                 //     ->where('selling_area_id', $sellingAreaId)
                 //     ->first();
@@ -359,7 +360,7 @@ class OrderService
                     // $menuData['price'] = $menuData['original_price'];
                     $menuData['status'] = 'pos_confirmed';
                     $menuData['area_id'] = $cookingAreaId;
-                    $menuData['inventory_id']=$inventoryId;
+                    $menuData['inventory_id'] = $inventoryId;
                     $menuData['sub_total_price'] = (isset($menuData['is_package']) && $menuData['is_package'])
                         ? 0
                         : ($menuData['original_price'] + $totalExtraPrice) - $defaultDiscountAmont; //after  
@@ -398,7 +399,7 @@ class OrderService
                     $menuData['date'] = now();
                     $menuData['quantity'] = $defaultQuantity;
                     $menuData['status'] = 'pos_confirmed';
-                    $menuData['inventory_id']=$inventoryId;
+                    $menuData['inventory_id'] = $inventoryId;
                     // $menuData['remark'] = $menuData['remark'];
                     // $menuData['original_price'] = $data['original_price'];
                     // $menuData['menu_id'] = $data['menu_id'];
@@ -482,7 +483,7 @@ class OrderService
             ->value('inventory_id');
 
         if (!$inventoryId) {
-            ResponseMessage('Inventory and Area are not related', 419);
+            ResponseMessage('Inventory and Area are not related,pls check', 419);
         }
 
         return $inventoryId;
@@ -510,14 +511,13 @@ class OrderService
 
     }
 
-    public function checkInventoryEnough($menuId, $quantity)
+    public function checkInventoryEnough($menuId, $inventoryId, $quantity)
     {
         // $inventoryId = UserData()->department->inventory->inventory_id;
-        $inventory = Inventory::where('name', 'Kitchen Inventory')->first();
-        if (!$inventory) {
-            ResponseMessage('Kitchen Inv not found', 404);
-        }
-        $inventoryId = $inventory->id;
+        // $inventory = Inventory::where('name', 'Kitchen Inventory')->first();
+        // if (!$inventory) {
+        //     ResponseMessage('Kitchen Inv not found', 404);
+        // }
         $menuStepItemByMenu = MenuStepItem::join('items', 'menu_step_items.item_id', 'items.id')
             ->whereHas('menuStep', function ($q) use ($menuId) {
                 $q->where('menu_id', $menuId)
@@ -546,13 +546,13 @@ class OrderService
 
     }
 
-    public function checkSellingExtraInventoryIsEnough($sellingExtraIds)
+    public function checkSellingExtraInventoryIsEnough($sellingExtraIds, $inventoryId)
     {
-        $inventory = Inventory::where('name', 'Kitchen Inventory')->first();
-        if (!$inventory) {
-            ResponseMessage('Kitchen Inv not found', 404);
-        }
-        $inventoryId = $inventory->id;
+        // $inventory = Inventory::where('name', 'Kitchen Inventory')->first();
+        // if (!$inventory) {
+        //     ResponseMessage('Kitchen Inv not found', 404);
+        // }
+        // $inventoryId = $inventory->id;
         foreach ($sellingExtraIds as $extraId) {
             $sellingExtra = SellingExtra::with('item')
                 ->find($extraId);
@@ -582,8 +582,8 @@ class OrderService
 
     public function actionInventoryItem($orderItem, $morphMapName, $action, $sellingExtraIds)
     {
-        $inventoryId = UserData()->department->inventory->inventory_id;
-        // $inventoryId=6; //fix kitchen
+        // $inventoryId = UserData()->department->inventory->inventory_id;
+        $inventoryId = $orderItem->inventory_id;
         $menuId = $orderItem->menu_id;
         $menuStepItemByMenu = MenuStepItem::join('items', 'menu_step_items.item_id', 'items.id')
             ->whereHas('menuStep', function ($q) use ($menuId) {
