abe652b020f6d3f338fe525f3bd195d0f423e5e3
Thet Htoo Htoo San <techxila.jp@gmail.com>
2025-09-13 12:41:50 +0630

    fix:noti

--- app/Events/SendDepartmentNotification.php
+++ app/Events/SendDepartmentNotification.php
@@ -3,6 +3,7 @@
 namespace App\Events;
 
 use App\Models\Notification;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
@@ -15,14 +16,15 @@ class SendDepartmentNotification implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $department_id;
+    public $user_ids;
     public $date;
     public $title;
     public $body;
 
-    public function __construct(Notification $notification, $department_id)
+    public function __construct(Notification $notification,  $user_ids)
     {
-        $this->department_id = $department_id;
+        $this->user_ids = is_array($user_ids) ? $user_ids : [$user_ids];
+        // $this->department_id = $department_id;
         $this->date = $notification->date_time;
         $this->title = $notification->title;
         $this->body = $notification->preview;
@@ -35,9 +37,28 @@ class SendDepartmentNotification implements ShouldBroadcast
      */
     public function broadcastOn(): array
     {
-        return [
-            new Channel("send-notification.department.{$this->department_id}"),
-        ];
+        // return [
+        //     new Channel("send-notification.department.{$this->department_id}"),
+        // ];
+
+        $channels = [];
+        
+        foreach ($this->user_ids as $user_id) {
+            $channels[] = new Channel("send-notification.staff.{$user_id}");
+        }
+        Log::info('SendDepartmentNotification broadcasting to channels', [
+            'channels' => array_map(function($channel) {
+                return $channel->name;
+            }, $channels),
+            'user_count' => count($this->user_ids),
+            'payload' => [
+                'date' => $this->date,
+                'title' => $this->title,
+                'body' => $this->body,
+            ]
+        ]);
+        return $channels;
+
     }
 
     public function broadcastWith()
