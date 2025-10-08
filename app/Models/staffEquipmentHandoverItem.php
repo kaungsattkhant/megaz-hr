91654456ee6fdd2b7ba45098552f6ad640b69374
Thet Htoo Htoo San <thethhsan@gmail.com>
2025-10-08 21:33:25 +0630

    fix model

--- app/Models/staffEquipmentHandoverItem.php
+++ app/Models/staffEquipmentHandoverItem.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class StaffEquipmentHandoverItem extends Model
+class staffEquipmentHandoverItem extends Model
 {
     use HasFactory;
 
