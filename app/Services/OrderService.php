5a929c31e3f2f380cc6ca061667ff15a3daa327e
kaungsattkhant <ookaung42@gmail.com>
2025-10-22 15:08:49 +0630

    doen

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -596,7 +596,8 @@ class OrderService
 
         if ($totalPacks == 0) {
             $menu=Menu::find($menuId);
-            ResponseMessage('No packs found for  '.$menu->name, 404);
+            $msg='No packs found for  '.$menu->name.'. Inventoy Id is '.$inventoryId.'. Total pack is '.$totalPacks;
+            ResponseMessage($msg, 404);
         }
 
         if ($totalPacks < $quantity) {
