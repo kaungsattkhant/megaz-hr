cf32b6ec2297bb9c9c6dadd0af49d466f180aa4f
kaungsattkhant <ookaung42@gmail.com>
2025-10-29 13:28:16 +0630

    done

--- app/Http/Requests/Admin/TransactionRequest.php
+++ app/Http/Requests/Admin/TransactionRequest.php
@@ -13,7 +13,7 @@ class TransactionRequest extends APIRequest
             'account_id'=>'required',
             'value'=>'required|integer',
             'action' => 'required|in:debit,credit',
-            'cash_account_id'=>'required|exists:accounts,id'
+            'cash_account_id'=>'required'
         ];
     }
     public function authorize()
