52eeb7ff00ef47a6f0e1f37ef76352fc830da584
kaungsattkhant <ookaung42@gmail.com>
2025-11-05 15:34:05 +0630

    done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,6 +29,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:bar-total-expense')->monthly();
         $schedule->command('app:kitchen-expense')->monthly();
         $schedule->command('app:kitchen-menu-total')->monthly();
+        $schedule->command('staff:auto-checkout')->everyMinute();
+
     }
 
 
