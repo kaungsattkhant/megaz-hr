f890c3435f68134e7103e3a464172deaf1e7dc9d
Phyo Kyaw San <phyojupiter9@gmail.com>
2025-11-17 19:01:52 +0630

    firebase config fixed in master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,7 +25,7 @@
     <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
-    
+
     <script src="https://cdn.ckeditor.com/4.22.1/standard/ckeditor.js"></script>
 
         <!-- Chart.js CDN -->
@@ -40,7 +40,7 @@
             <img src="{{asset('img/logo.png')}}" alt="Loading..." width="100">
         </div>
     </div> -->
-    
+
 
      <script type="module">
         // Import the functions you need from the SDKs you need
@@ -53,13 +53,13 @@
         // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
         const firebaseConfig = {
-            apiKey: "AIzaSyA_RiMlzMDYxKF_iT8wBxPAW3NpKxEPxas",
-            authDomain: "megaz-project.firebaseapp.com",
-            projectId: "megaz-project",
-            storageBucket: "megaz-project.appspot.com",
-            messagingSenderId: "25949618078",
-            appId: "1:25949618078:web:86369862c2a1fc084ca08f",
-            measurementId: "G-M6MF9D78DD"
+            apiKey: "AIzaSyCvUQJCbpIMC02ceunLnZI7Dyq5VrEjw2M",
+            authDomain: "megaz-78046.firebaseapp.com",
+            projectId: "megaz-78046",
+            storageBucket: "megaz-78046.firebasestorage.app",
+            messagingSenderId: "144736503266",
+            appId: "1:144736503266:web:50908d34738ad48d93a5bb",
+            measurementId: "G-20WGQM0S3T"
         };
 
         // Initialize Firebase
@@ -199,7 +199,7 @@
                 });
             }
         }
-        
+
         // console.log(currentPath)
         $(window).on('load', function() {
             scrollToActiveLink();
