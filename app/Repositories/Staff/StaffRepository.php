81745b0ba7a498d4506e89c93a2af9297c0765df
kaungsattkhant <ookaung42@gmail.com>
2025-12-04 12:09:25 +0630

    add profile image

--- app/Repositories/Staff/StaffRepository.php
+++ app/Repositories/Staff/StaffRepository.php
@@ -111,6 +111,7 @@ class StaffRepository implements StaffRepositoryInterface
             $data['is_active'] = 1;
             $data = RemoveNullValues($data);
             $staff = Staff::create($data);
+            dd($staff); 
 
             if (isset($data['certificate_images']) && is_array($data['certificate_images'])) {
                 foreach ($data['certificate_images'] as $certificateImage) {
