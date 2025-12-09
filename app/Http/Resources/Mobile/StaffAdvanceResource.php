7d805fbc2350bb5799e1ee1e1a9e15465ba2d722
kaungsattkhant <ookaung42@gmail.com>
2025-12-09 11:06:02 +0630

    fix:advance response

--- app/Http/Resources/Mobile/StaffAdvanceResource.php
+++ app/Http/Resources/Mobile/StaffAdvanceResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Mobile;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,6 +19,7 @@ class StaffAdvanceResource extends JsonResource
             'id'=>$this->id,
             'advance_ref_no'=>$this->advance_ref_no,
             'date_time'=>$this->date_time,
+            'month'=>Carbon::parse($this->date_time)->format('F'),
             "remaining_amount"=>$this->remaining_amount,
             'advance_payment'=> $this->advance_payment->map(function ($payment) {
                 return [
