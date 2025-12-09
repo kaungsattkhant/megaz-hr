41deb4ba12daed5dd6c4e7a9aecb4a24c436c08e
kaungsattkhant <ookaung42@gmail.com>
2025-12-09 10:33:47 +0630

    fixed

--- app/Repositories/Salary/SalaryRepository.php
+++ app/Repositories/Salary/SalaryRepository.php
@@ -499,10 +499,6 @@ class SalaryRepository implements SalaryRepositoryInterface
     $data = Overtime::with(['overtimeCategory', 'timeShift.shift'])
       ->where('staff_id', $staffId)->orderBy('id', 'desc')
       ->paginate(config('common.list_count'));
-
-    if ($data->isEmpty()) {
-      ResponseMessage('Overtime not found.', 404);
-    }
     ResponseData($data);
   }
 
