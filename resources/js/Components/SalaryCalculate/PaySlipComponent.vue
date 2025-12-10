f697e2b4cd5bd151c23061ad223f1284c7138888
Nevarnar <loverhein2011@gmail.com>
2025-12-10 14:54:52 +0630

    fix confirm color

--- resources/js/Components/SalaryCalculate/PaySlipComponent.vue
+++ resources/js/Components/SalaryCalculate/PaySlipComponent.vue
@@ -118,7 +118,8 @@
                                     <td class="whitespace-nowrap">
                                         {{ salary.net_salary }}
                                     </td>
-                                    <td>
+                                    
+                                    <td class="whitespace-nowrap">
                                         <i class="far fa-check-double" :class="salary.is_confirm === 1 ? 'text-green-600' : 'text-gray-500'"></i>
                                     </td>
                                     <td class="whitespace-nowrap">
