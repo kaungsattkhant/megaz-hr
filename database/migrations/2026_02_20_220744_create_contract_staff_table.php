406bebed40fe12db0ce48ce1696aff8338b99d4f
kaungsattkhant <ookaung42@gmail.com>
2026-02-21 13:14:52 +0630

    fixed

--- database/migrations/2026_02_20_220744_create_contract_staff_table.php
+++ database/migrations/2026_02_20_220744_create_contract_staff_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\ContractStaffEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,13 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('contract_id');
             $table->unsignedBigInteger('staff_id');
+            $table->string('signed_document')->nullable();
+            $table->dateTime('signed_at')->nullable();
+            $table->unsignedInteger('confirmed_by')->nullable();
+            $table->dateTime('confirmed_at')->nullable();   
+            $table->unsignedInteger('cancelled_by')->nullable();
+            $table->dateTime('cancelled_at')->nullable();
+            $table->enum('status',ContractStaffEnum::getValues())->default(ContractStaffEnum::DRAFT->value);
             $table->timestamps();
         });
     }
