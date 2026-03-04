71e9cb03cff672692a0a1967874931dafba55dcd
Nevarnar <loverhein2011@gmail.com>
2026-03-04 21:42:20 +0630

    remove role valid in okr edit

--- resources/js/Components/OKR/OkrEditComponent.vue
+++ resources/js/Components/OKR/OkrEditComponent.vue
@@ -340,10 +340,10 @@ export default {
                 this.alertValidationMessage(`Objective Name`);
                 return 1;
             }
-            else if(!this.selectedRole){
-                this.alertValidationMessage(`Role`);
-                return 1;
-            }
+            // else if(!this.selectedRole){
+            //     this.alertValidationMessage(`Role`);
+            //     return 1;
+            // }
             else if(!this.selectedSop){
                 this.alertValidationMessage(`SOP`);
                 return 1;
