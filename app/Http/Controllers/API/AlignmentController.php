96194ce3456fe32a1fc9cd44034ae35edeb9f258
kaungsattkhant <ookaung42@gmail.com>
2026-03-19 09:45:53 +0630

    fixed:alignemnt list

--- app/Http/Controllers/API/AlignmentController.php
+++ app/Http/Controllers/API/AlignmentController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Alignment;
 use Illuminate\Http\Request;
 
 class AlignmentController extends Controller
 {
     //
+    public function index(Request $request){
+        $alignments=Alignment::orderBy('id','asc')->get();
+        \ResponseData($alignments);
+    }
 }
