bd98c1954b391c27f5e0fbb99c9168c0161c9656
kaungsattkhant <ookaung42@gmail.com>
2026-03-24 15:21:21 +0630

    done

--- database/migrations/2026_02_24_122354_add_priority_to_objectives_table.php
+++ database/migrations/2026_02_24_122354_add_priority_to_objectives_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('objectives', function (Blueprint $table) {
             //
-            $table->integer('priority')->nullable();
+            // $table->integer('priority')->nullable();
         });
     }
 
